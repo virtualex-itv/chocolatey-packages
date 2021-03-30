add736747425ec4a0764fd6f31762fb68469bc6f
virtualex <virtualex-itv@users.noreply.github.com>
2021-03-30 19:39:59 +0000

    AU: 2 updated - lens termius [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/27726dbc4f07288cc7a1059915bc1b3dde110389

--- automatic/lens/tools/chocolateyinstall.ps1
+++ automatic/lens/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://github.com/lensapp/lens/releases/download/v4.1.5/Lens-Setup-4.1.5.exe'
+$checksum               = '1a8b7015960c04e679e0029259b84974fa03beb1a9f64212900a983132c2e857'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
--- automatic/termius/tools/chocolateyinstall.ps1
+++ automatic/termius/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://autoupdate.termius.com/windows/Termius.exe'
+$checksum               = '941694821D534B189507A08A92546BD140658CB7FE03A057BD64B6D2E1F91DDE'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
