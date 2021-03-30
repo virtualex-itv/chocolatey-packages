74742e0e7f53acd2268d4d2f7502244462d2c873
virtualex <virtualex-itv@users.noreply.github.com>
2021-03-30 19:56:35 +0000

    AU: 1 updated - vmware-horizon-client [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/355c5fd9b2d770ef2e06e96ddb744e862607cd51

--- automatic/vmware-horizon-client/tools/chocolateyinstall.ps1
+++ automatic/vmware-horizon-client/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://download3.vmware.com/software/view/viewclients/CART22FQ1/VMware-Horizon-Client-2103-8.2.0-17759012.exe'
+$checksum               = '1ffbdab7b88c0527b8f5c05540f92fbf74dd2d804fa20fe328cc18c0bc886fc9'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
