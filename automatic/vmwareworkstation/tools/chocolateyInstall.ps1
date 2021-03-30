d5eccb9f18af5b448bd4e5a4f796c6569996f7b0
virtualex <info@ithinkvirtual.com>
2021-03-30 15:35:45 -0400

    (vmwareworkstation) new package

---
+++ automatic/vmwareworkstation/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference  = 'Stop'
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$zippackageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+
+}
+
+Install-ChocolateyZipPackage @zippackageArgs
+
+$fileLocation = Get-ChildItem $toolsDir\VMware-workstation*.exe
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  file                  = $fileLocation
+  softwareName          = 'VMware Workstation*'
+  silentArgs            = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
+  validExitCodes        = @(0, 3010, 1614, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ErrorAction SilentlyContinue; if (Test-Path $_) { Set-Content "$_.ignore" } }
