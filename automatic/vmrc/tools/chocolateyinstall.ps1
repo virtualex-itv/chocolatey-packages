7e35efabd795636dd704f09b4f22ebcf2c44945a
virtualex <info@ithinkvirtual.com>
2021-03-30 15:33:29 -0400

    (vmrc) new package

---
+++ automatic/vmrc/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$zippackageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+
+}
+
+Install-ChocolateyZipPackage @zippackageArgs
+
+$fileLocation = Get-ChildItem $toolsDir\VMware-VMRC*.exe
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  file                  = $fileLocation
+  softwareName          = 'VMware Remote Console*'
+  silentArgs            = '/s /v /qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=1 DATACOLLECTION=0 REBOOT=ReallySuppress'
+  validExitCodes        = @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ErrorAction SilentlyContinue; if (Test-Path $_) { Set-Content "$_.ignore" } }
---
+++ automatic/vmrc/update.ps1
@@ -0,0 +1,68 @@
+Import-Module AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+$releases = 'https://softwareupdate.vmware.com/cds/vmw-desktop/vmrc/'
+
+function global:au_GetLatest {
+  #region Get VMware Remote Console Url
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $versionFolder = $download_page.Links | Where-Object { $_.href -Match '(\d+)' } | Select-Object -Last 1 -ExpandProperty href
+  $versionFolderUrl = $releases + $versionFolder
+
+  $build_page = Invoke-WebRequest -Uri $versionFolderUrl -UseBasicParsing
+  $buildFolder = $build_page.Links | Where-Object { $_.href -Match '(\d+)' } | Select-Object -Last 1 -ExpandProperty href
+  $buildFolderUrl = $versionFolderUrl + $buildFolder
+
+  $staticFolder = 'windows/'
+  $fileFolderUrl = $buildFolderUrl + $staticFolder
+
+  $re = '\.tar$'
+  $file_page = Invoke-WebRequest -Uri $fileFolderUrl -UseBasicParsing
+  $fileName = $file_page.Links | Where-Object { $_.href -match $re } | Select-Object -ExpandProperty href
+
+  $Url32 = $fileFolderUrl + $fileName
+  $version = $versionFolder.Replace("/",".") + $buildFolder.Replace("/","")
+  $ChecksumType = 'sha256'
+  #endregion
+
+  #region Get Release Notes Url
+  $feed = 'https://docs.vmware.com/en/VMware-Remote-Console/rn_rss.xml'
+  $xml_fileName = Split-Path -Leaf $feed
+  $dest = "$env:TEMP\$xml_fileName"
+
+  Get-WebFile $feed $dest | Out-Null
+  [xml]$content = Get-Content $dest
+
+  $ReleaseNotes = $content.feed.entry | Where-Object { $_.id -Match $versionFolder } | Select-Object -ExpandProperty id
+
+  Remove-Item $dest -Force -ErrorAction SilentlyContinue
+  #endregion
+
+  @{
+    Url32             = $Url32
+    Version           = $version
+    ChecksumType32    = $ChecksumType
+    ReleaseNotes      = $ReleaseNotes
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32 -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_SearchReplace {
+  @{
+      'tools\chocolateyInstall.ps1' = @{
+          "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+          "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+          "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      }
+  }
+}
+
+function global:au_AfterUpdate {
+  Update-Metadata -key "releaseNotes" -value $Latest.ReleaseNotes
+}
+
+Update-Package -ChecksumFor none
