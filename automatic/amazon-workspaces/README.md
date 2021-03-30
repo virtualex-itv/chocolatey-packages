6566ee1c9517a630599a35780078d807e0809bc5
virtualex <info@ithinkvirtual.com>
2021-03-30 15:21:02 -0400

    (amazon-workspaces) new package

---
+++ automatic/amazon-workspaces/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/7d89ac1ff4fbd6f9dc60afe9dfe95ae0f209144c/icons/amazon-workspaces.png" width="48" height="48"/> [amazon-workspaces](https://chocolatey.org/packages/amazon-workspaces)
+
+Amazon WorkSpaces plays nice with everyone. Access your personal Windows environment on Android, iOS, Fire, Mac, PC and Chromebook devices.
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/amazon-workspaces/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'msi'
+  softwareName          = "Amazon WorkSpaces*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/qn REBOOT=ReallySuppress'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
