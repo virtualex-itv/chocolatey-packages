c9d2ae3bd7781147a36aa0da3659a7ff02da3966
virtualex <info@ithinkvirtual.com>
2021-04-02 17:37:11 -0400

    (vmware-workstation-player) Updated some files

--- automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
+++ automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
@@ -2,33 +2,20 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://softwareupdate.vmware.com/cds/vmw-desktop/player/16.1.1/17801498/windows/core/VMware-player-16.1.1-17801498.exe.tar'
-$checksum               = '3d85dfc810a2d5f3bb7b23c141b4af512be71a98f6beaf257b693767145d0137'
+$url                    = 'https://download3.vmware.com/software/player/file/VMware-player-16.1.1-17801498.exe'
+$checksum               = 'd43b11a651d36e4113105de24630413c3eea904b835282e4978b95a2c6a205b3'
 $checksumType           = 'sha256'
-
-$zippackageArgs = @{
-  packageName           = $env:ChocolateyPackageName
-  unzipLocation         = $toolsDir
-  url                   = $url
-  checksum              = $checksum
-  checksumType          = $checksumType
-
-}
-
-Install-ChocolateyZipPackage @zippackageArgs
-
-$fileLocation = Get-ChildItem $toolsDir\VMware-player*.exe
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
   unzipLocation         = $toolsDir
   fileType              = 'exe'
-  file                  = $fileLocation
-  softwareName          = 'VMware Player*'
+  url                   = $url
+  softwareName          = "VMware Player*"
+  checksum              = $checksum
+  checksumType          = $checksumType
   silentArgs            = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes        = @(0, 3010, 1614, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
-
-Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ErrorAction SilentlyContinue; if (Test-Path $_) { Set-Content "$_.ignore" } }
