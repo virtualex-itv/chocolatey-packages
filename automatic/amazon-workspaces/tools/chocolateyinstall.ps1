97bebf8d13ed887ddfab16e86d1d992df0eacc84
virtualex <virtualex-itv@users.noreply.github.com>
2021-04-02 21:45:52 +0000

    AU: 1 updated - amazon-workspaces [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/1ded5856ce6ed82c7c16df13623a18067e494676

--- automatic/amazon-workspaces/tools/chocolateyinstall.ps1
+++ automatic/amazon-workspaces/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
-$checksum               = '604E40ED03883E1D740367FBCC4DA9C21FC88FE26F9DF7ACA4ED0A25C0B1AC2E'
+$checksum               = '46D220FF1C206B246AD2EB2456154606298191C9CD2D2FF374384944CA88199F'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
