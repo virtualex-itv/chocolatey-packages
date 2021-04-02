16bb5ef7fc37bc6e714accb6b8ca35e3c5aee082
virtualex <info@ithinkvirtual.com>
2021-04-02 15:43:31 -0400

    (vmware-horizon-client ) Updated update.ps1

--- automatic/vmware-horizon-client/update.ps1
+++ automatic/vmware-horizon-client/update.ps1
@@ -1,14 +1,9 @@
 Import-Module AU
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 $allProducts = 'https://my.vmware.com/channel/public/api/v1.0/products/getAllProducts?locale=en_US&isPrivate=true'
 
-function global:au_GetLatest {
-  #region Get VMware Horizon Clients for Windows Url
-  $jsonProducts = Invoke-WebRequest -Uri $allProducts | ConvertFrom-Json
-
-  $re = 'vmware_horizon_clients'
-  $productName = ($jsonProducts.productCategoryList.productList.actions | Where-Object target -match $re | Select-Object -First 1 -ExpandProperty target).Split("/")[-1]
+function CreateStream {
+  param ( $productName )
 
   $productBinaries = "https://my.vmware.com/channel/public/api/v1.0/products/getRelatedDLGList?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=$($productName)&dlgType=PRODUCT_BINARY"
 
@@ -22,31 +17,52 @@ function global:au_GetLatest {
   $jsonFile = Invoke-WebRequest -Uri $downloadFiles | ConvertFrom-Json
 
   $Url32 = $jsonFile.downloadFiles.thirdPartyDownloadUrl
-  $version = ($Url32).Split("-")[-2] + "." + ($Url32).Split("-")[-1] -replace (".exe", "")
+  $version = ($Url32).Split("-")[-2] + '.' + ($Url32).Split("-")[-1] -replace (".exe", "")
   $ChecksumType = 'sha256'
   $checksum = $jsonFile.downloadFiles.sha256checksum
   #endregion
 
   #region Get Release Notes Url
-  $feed = 'https://docs.vmware.com/en/VMware-Horizon-Client-for-Windows/rn_rss.xml'
-  $xml_fileName = Split-Path -Leaf $feed
-  $dest = "$env:TEMP\vhc_$xml_fileName"
+  $dlgHeader = "https://my.vmware.com/channel/public/api/v1.0/products/getDLGHeader?locale=en_US&downloadGroup=$($product.code)&productId=$($product.productId)"
 
-  Get-WebFile $feed $dest | Out-Null
-  [xml]$content = Get-Content $dest
+  $jsonHeader = Invoke-WebRequest -Uri $dlgHeader | ConvertFrom-Json
 
-  $ReleaseNotes = $content.feed.entry | Where-Object { $_.id -match $jsonFile.downloadFiles.version } | Select-Object -ExpandProperty id
-
-  Remove-Item $dest -Force -ErrorAction SilentlyContinue
+  $ReleaseNotes = ($jsonHeader.dlg.documentation).Split(';|&') | Where-Object { $_ -match '.html' }
   #endregion
 
-  @{
+  $Result = @{
     Url32          = $Url32
     Version        = $version
     ChecksumType32 = $ChecksumType
     Checksum32     = $checksum
     ReleaseNotes   = $ReleaseNotes
   }
+  return $Result
+}
+
+function global:au_GetLatest {
+  $streams = @{}
+
+  #region Get VMware Horizon Client for Windows Url
+  $jsonProducts = Invoke-WebRequest -Uri $allProducts | ConvertFrom-Json
+
+  $re = 'vmware_horizon_clients'
+  $productName = ($jsonProducts.productCategoryList.productList.actions | Where-Object target -match $re | Select-Object -First 1 -ExpandProperty target).Split("/")[-1]
+
+  $productHeaderUrl = "https://my.vmware.com/channel/public/api/v1.0/products/getProductHeader?locale=en_US&category=desktop_end_user_computing&product=vmware_horizon_clients&version=$($productName)"
+
+  $jsonProductHeader = Invoke-WebRequest -Uri $productHeaderUrl | ConvertFrom-Json
+
+  foreach ( $id in $jsonProductHeader.versions.id ) {
+    $streams.Add( $id, ( CreateStream $id ) )
+  }
+
+  return @{ Streams = $streams }
+
+  #fetch all product ids
+  #loop over those product ids and invoke new function
+  #with results, add to streams HashTable
+  #return HashTable to AU
 }
 
 function global:au_SearchReplace {
