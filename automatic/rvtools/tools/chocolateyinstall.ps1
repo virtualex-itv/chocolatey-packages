e8afee24dcd110d1a687db095b41d6412d721015
virtualex <virtualex-itv@users.noreply.github.com>
2021-04-05 12:01:36 +0000

    AU: 2 updated - rvtools termius [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/09d7bb9d4efe6efd029bff31fb54653c5cd11f9b

--- automatic/rvtools/tools/chocolateyinstall.ps1
+++ automatic/rvtools/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.0.7.msi'
-$checksum               = '1dc112e0ced9b6cdeceaa6d0de008a312ca840c2ff42e7dc30db02f4d2f943ab'
+$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.1.2.msi'
+$checksum               = 'bdd1ea272d534db1ab21be5a8164e93c77beeb23315f3838369902fd626cbf51'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
--- automatic/termius/tools/chocolateyinstall.ps1
+++ automatic/termius/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://autoupdate.termius.com/windows/Termius.exe'
-$checksum               = '941694821D534B189507A08A92546BD140658CB7FE03A057BD64B6D2E1F91DDE'
+$checksum               = '5A3FC1C3C1F4B3BCFB5DEDEB43A569BA7EA2D46B28C6CA39B2018C406DBE0FE7'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
