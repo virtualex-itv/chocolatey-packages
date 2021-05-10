392e583a57989540fd1cb57581cac08c2d934587
virtualex <virtualex-itv@users.noreply.github.com>
2021-05-10 12:04:32 +0000

    AU: 1 updated - laps [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/bfe1510b9eba922463dbf4a8d9c4a753fd7f8fa6

--- automatic/laps/tools/chocolateyinstall.ps1
+++ automatic/laps/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $toolsDir            = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                 = 'https://download.microsoft.com/download/C/7/A/C7AAD914-A8A6-4904-88A1-29E657445D03/LAPS.x86.msi'
-$checksum            = '9f0fa541b472c20508973f561b0d7850a7bf779c8459f9e33471083619fd6eda'
+$checksum            = 'fddab92f98e08e2cae0e025f4cf2cdd5f0d87c8dfc5719045933d8f0130aff4d'
 $checksumType        = 'sha256'
 $url64               = 'https://download.microsoft.com/download/C/7/A/C7AAD914-A8A6-4904-88A1-29E657445D03/LAPS.x64.msi'
-$checksum64          = 'f63ebbc45e2d080630bd62a195cd225de734131a56bb7b453c84336e37abd766'
+$checksum64          = '0001dd763cc74d37e3979e016ffcd0512d91494a0b3b7270c7a3bb4e1915f6d1'
 $checksumType64      = 'sha256'
 $pp                  = Get-PackageParameters
 
