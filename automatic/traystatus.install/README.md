d56d7a1786594cd260dcc520544818c3bb2f2a45
virtualex <info@ithinkvirtual.com>
2021-05-17 14:56:20 -0400

    (traystatus.install) New AU package

---
+++ automatic/traystatus.install/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/30ad93c0ae8f036a00bfbdffbd16b8aa2ddd9c49/icons/traystatus.png" width="48" height="48"/> [traystatus.install](https://chocolatey.org/packages/traystatus.install)
+
+TrayStatus shows you the status of keyboard keys like Caps Lock, Num Lock, Scroll Lock, Alt, Ctrl, and more, right in your system tray.
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/traystatus.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatusSetup-4.5.exe'
+$checksum              = '22620cdac1077822a8ed0dbb532d862aa6426e22744d4aa844edf3bacf14009f'
+$checksumType          = 'sha256'
+
+$packageArgs = @{
+  packageName          = $env:ChocolateyPackageName
+  unzipLocation        = $toolsDir
+  fileType             = 'exe'
+  softwareName         = 'TrayStatus*'
+  url                  = $url
+  checksum             = $checksum
+  checksumType         = $checksumType
+  silentArgs           = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes       = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
