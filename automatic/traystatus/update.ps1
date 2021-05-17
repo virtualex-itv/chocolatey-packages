f8310c80859f3b66ccbc3385ad7b3b6e4ad2afe2
virtualex <info@ithinkvirtual.com>
2021-05-17 14:55:38 -0400

    (traystatus) New AU package

---
+++ automatic/traystatus/update.ps1
@@ -0,0 +1,30 @@
+Import-Module AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+$releases = 'https://www.traystatus.com/Download'
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = 'Log='
+  $url = $download_page.Links | Where-Object { $_.href -match $re } | Select-Object -First 1 -ExpandProperty href
+  $url32 = Get-RedirectedUrl $url
+
+  $re = 'Setup-|\.exe$'
+  $version = ($url32 -split $re)[1]
+
+  @{
+    Url32            = $Url32
+    Version          = $version
+  }
+}
+
+function global:au_SearchReplace {
+  @{ }
+}
+
+function global:au_AfterUpdate {
+  Update-Metadata -key "dependency" -value "TrayStatus.Install|[$($Latest.Version)]"
+}
+
+Update-Package -ChecksumFor none
