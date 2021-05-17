dd4095f09203c15f20309de1293b70a835aaf668
virtualex <info@ithinkvirtual.com>
2021-05-17 14:57:19 -0400

    (traystatus.portable) New AU package

---
+++ automatic/traystatus.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.5.zip'
+$checksum              = '69c4078ddc7d64223f368f2bc86477d6a8e74e7c9b133a5a5614e7a468334570'
+$checksumType          = 'sha256'
+
+$packageArgs = @{
+  packageName          = $env:ChocolateyPackageName
+  unzipLocation        = $toolsDir
+  fileType             = 'exe'
+  softwareName         = 'TrayStatus*'
+  url                  = $url
+  checksum             = $checksum
+  checksumType         = $checksumType
+  silentArgs           = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes       = @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/traystatus.portable/update.ps1
@@ -0,0 +1,38 @@
+Import-Module AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+$releases = 'https://www.traystatus.com/Download'
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = 'Log='
+  $url = $download_page.Links | Where-Object { $_.href -match $re } | Select-Object -Skip 1 -First 1 -ExpandProperty href
+  $url32 = Get-RedirectedUrl $url
+
+  $re = '-|\.zip$'
+  $version = ($url32 -split $re)[1]
+  $ChecksumType = 'sha256'
+
+  @{
+    Url32            = $Url32
+    Version          = $version
+    ChecksumType32   = $ChecksumType
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32 -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_SearchReplace {
+  @{
+      'tools\chocolateyInstall.ps1' = @{
+          "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+          "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+          "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      }
+  }
+}
+
+Update-Package -ChecksumFor none
