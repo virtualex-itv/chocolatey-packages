a9c99355777a4f1f7dce5ba9be8137b9345655b4
virtualex <virtualex-itv@users.noreply.github.com>
2021-05-18 02:01:38 +0000

    AU: 1 updated - shellsend [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/7a89fd376b08c460038d576a8394a4dc0758b495

--- automatic/shellsend/tools/chocolateyinstall.ps1
+++ automatic/shellsend/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = ''
-$checksum              = ''
-$checksumType          = ''
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/111/ShellSend-2.1.zip'
+$checksum              = '2ab484c88659dac991e47a5ba4316391a56c6139016904252a03c248c12459b3'
+$checksumType          = 'sha256'
 
 $packageArgs = @{
   packageName          = $env:ChocolateyPackageName
