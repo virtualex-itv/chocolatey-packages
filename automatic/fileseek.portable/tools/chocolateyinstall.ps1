86ad33ce4d5f85b99552ddc30b3838927c6c8ee0
virtualex <virtualex-itv@users.noreply.github.com>
2021-05-18 11:41:01 +0000

    AU: 1 updated - fileseek.portable [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/fc0146c7bc634f4adce777de8f0a0ac5471aea96

--- automatic/fileseek.portable/tools/chocolateyinstall.ps1
+++ automatic/fileseek.portable/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = ''
-$checksum              = ''
-$checksumType          = ''
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/103/FileSeek-6.6.zip'
+$checksum              = 'e2c5114d0f523316cd3504a1b2a2f50a720f3bba37688382035f245304d2fd97'
+$checksumType          = 'sha256'
 
 $packageArgs = @{
   packageName          = $env:ChocolateyPackageName
