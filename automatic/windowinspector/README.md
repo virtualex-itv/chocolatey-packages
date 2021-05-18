ec4fc96a7ed14eec9fec08f2f301eccbbce68a09
virtualex <info@ithinkvirtual.com>
2021-05-17 22:41:56 -0400

    (windowinspector) New AU package

---
+++ automatic/windowinspector/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/cfd5955b3d908853bac0149541a3350ef83f1a7c/icons/windowinspector.png" width="48" height="48"/> [windowinspector](https://chocolatey.org/packages/windowinspector)
+
+View the window class, text, properties, and more, simply by moving your mouse cursor over the target window.
+
+**Please Note**: This is an automatically updated package. If you find it is
+out of date by more than a day or two, please contact the maintainer(s) and
+let them know the package is no longer updating correctly.
---
+++ automatic/windowinspector/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                   = ''
+$checksum              = ''
+$checksumType          = ''
+
+$packageArgs = @{
+  packageName          = $env:ChocolateyPackageName
+  unzipLocation        = $toolsDir
+  fileType             = 'exe'
+  softwareName         = 'WindowInspector*'
+  url                  = $url
+  checksum             = $checksum
+  checksumType         = $checksumType
+  silentArgs           = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes       = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
