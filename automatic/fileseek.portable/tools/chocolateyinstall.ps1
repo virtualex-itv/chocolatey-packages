c69d5c21a8f1703ac3a05afb8613f565a587427f
virtualex <info@ithinkvirtual.com>
2021-05-25 11:18:51 -0400

    [fileseek.portable:6.6]

--- automatic/fileseek.portable/tools/chocolateyinstall.ps1
+++ automatic/fileseek.portable/tools/chocolateyinstall.ps1
@@ -9,13 +9,9 @@ $checksumType          = 'sha256'
 $packageArgs = @{
   packageName          = $env:ChocolateyPackageName
   unzipLocation        = $toolsDir
-  fileType             = 'exe'
-  softwareName         = 'FileSeek*'
   url                  = $url
   checksum             = $checksum
   checksumType         = $checksumType
-  silentArgs           = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
-  validExitCodes       = @(0, 3010, 1641)
 }
 
 Install-ChocolateyZipPackage @packageArgs
