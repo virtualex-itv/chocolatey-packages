b787d550b989b7917a694125eb7cad4eb207f359
virtualex <virtualex-itv@users.noreply.github.com>
2021-06-18 12:03:13 +0000

    AU: 1 updated - vmware-tools [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/781a22be2846e8354707c67fb7c419d02731f18a

--- automatic/vmware-tools/tools/ChocolateyInstall.ps1
+++ automatic/vmware-tools/tools/ChocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://packages.vmware.com/tools/releases/11.2.6/windows/x86/VMware-tools-11.2.6-17901274-i386.exe'
-$checksum              = '4939583977e2467733e56841164070b59ad9293425c698d498397b88b6aa8221'
+$url                   = 'https://packages.vmware.com/tools/releases/11.3.0/windows/x86/VMware-tools-11.3.0-18090558-i386.exe'
+$checksum              = 'a8de439939aff185a0f5b2ff8682e0002b9c70a2409490643715425f5af653b2'
 $ChecksumType          = 'sha256'
-$url64                 = 'https://packages.vmware.com/tools/releases/11.2.6/windows/x64/VMware-tools-11.2.6-17901274-x86_64.exe'
-$checksum64            = '86589b458fc0ceda787f4ac019db29da859d2b6b382d1f20ddd4e8226235fdde'
+$url64                 = 'https://packages.vmware.com/tools/releases/11.3.0/windows/x64/VMware-tools-11.3.0-18090558-x86_64.exe'
+$checksum64            = 'e89dabd07d27cbbb70d8370ed1cdcf527c59663f7a69514f4ff164055270f184'
 $ChecksumType64        = 'sha256'
 
 $pp                    = Get-PackageParameters
