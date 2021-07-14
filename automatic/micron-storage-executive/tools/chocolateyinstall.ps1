ad0f6248e600d1d4680fe9c97a125cd64eb11300
virtualex <virtualex-itv@users.noreply.github.com>
2021-07-14 18:05:43 +0000

    AU: 1 updated - micron-storage-executive [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/5c47c9e20d0a0ec15d8a485053ecc3c336d838d6

--- automatic/micron-storage-executive/tools/chocolateyinstall.ps1
+++ automatic/micron-storage-executive/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://media-www.micron.com/-/media/client/global/documents/products/software/storage-executive-software/storageexecutive_windows.exe?rev=86438fde54e44942a39cde2dd9f1b134'
-$checksum               = '32e4d4febdccd3da328265bee1d049173a50d032a62253c8f1ed2842187706e3'
+$url                    = 'https://media-www.micron.com/-/media/client/global/documents/products/software/storage-executive-software/storageexecutive_windows.exe?rev=ff8f92214db640ea8051de118abc1177'
+$checksum               = 'b504becb91a75f2916ea84704b36888d87382f48d49f17801fe6b4d6629237f5'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
