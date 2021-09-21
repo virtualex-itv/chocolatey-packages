2aabe47dddc5bea9020abc397306857d0700d9ab
virtualex <virtualex-itv@users.noreply.github.com>
2021-09-21 12:04:52 +0000

    AU: 2 updated - lens termius [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/9abd9413662ad3e84d9482e5a8c12269ab2418d9

--- automatic/termius/tools/chocolateyinstall.ps1
+++ automatic/termius/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://autoupdate.termius.com/windows/Termius.exe'
-$checksum               = 'E3F54268851C470FAE994D4594FBB73135AF8867210506E16B09EC0AA700B97F'
+$checksum               = 'A77B652B7EB844A8FB78538A5E059C4F86D28516C6F2406801AFA6DD4F851E7C'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
