bfb4dc338850089f9d3ef4d87bddaba02ba04599
virtualex <virtualex-itv@users.noreply.github.com>
2021-10-14 18:03:55 +0000

    AU: 2 updated - vmwareworkstation vmware-workstation-player [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/bd2199d68e6446340cac43d81a6d5f3cf5d41a93

--- automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
+++ automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://download3.vmware.com/software/player/file/VMware-player-16.1.2-17966106.exe'
-$checksum               = 'ce5949c2ca89c6fc8349d63e6d1dd053325b5803b93870aa3b73a106d76c942f'
+$url                    = 'https://download3.vmware.com/software/player/file/VMware-player-full-16.2.0-18760230.exe'
+$checksum               = 'b87ab818e2cff09a87ebf765659b989e5046d44499b83eee67697d96dfd73623'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
--- automatic/vmwareworkstation/tools/chocolateyInstall.ps1
+++ automatic/vmwareworkstation/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-16.1.2-17966106.exe'
-$checksum               = '71b44f2fcfde663195b833ba19f2f70d9ed21a78f9bce35cf13c7f780418a336'
+$url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-16.2.0-18760230.exe'
+$checksum               = '39ce3a7e7731f9e260f2acb3a077b65967b0810332042ec169425ba5bc7dd21d'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
