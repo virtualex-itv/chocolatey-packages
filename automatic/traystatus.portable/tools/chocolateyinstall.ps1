d7322910effce69da4fb9863df06625f3dd6095e
virtualex <info@ithinkvirtual.com>
2021-12-18 10:10:03 -0500

    (traystatus.portable) manual bump

--- automatic/traystatus.portable/tools/chocolateyinstall.ps1
+++ automatic/traystatus.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.5.zip'
-$checksum              = '69c4078ddc7d64223f368f2bc86477d6a8e74e7c9b133a5a5614e7a468334570'
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.6.zip'
+$checksum              = '132485cd0deeac56762dfd08d14ff23219801b6682250ee3e1b3c691d159cb42'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
