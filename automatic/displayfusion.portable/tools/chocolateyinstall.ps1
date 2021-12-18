9bad81edc3d888f9370aec97d70d1d6196fc0d2b
virtualex <info@ithinkvirtual.com>
2021-12-18 10:06:57 -0500

    (displayfusion.portable) manual bump

--- automatic/displayfusion.portable/tools/chocolateyinstall.ps1
+++ automatic/displayfusion.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/101/DisplayFusion-9.8.zip'
-$checksum              = '8e6efc56ed0dd827c021e57df4594424a59844843eb9203af7b93a3f64d64c96'
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/101/DisplayFusion-9.9.zip'
+$checksum              = 'b6624f00a6e271f7b0d49d3521fac4cf46f5701df5a6474230de83230379a356'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
