488b7bfe3da289ad9237889efdf9352a381a9869
virtualex <info@ithinkvirtual.com>
2021-12-18 10:07:55 -0500

    (fileseek.portable) manual bump

--- automatic/fileseek.portable/tools/chocolateyinstall.ps1
+++ automatic/fileseek.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/103/FileSeek-6.6.zip'
-$checksum              = 'e2c5114d0f523316cd3504a1b2a2f50a720f3bba37688382035f245304d2fd97'
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/103/FileSeek-6.7.zip'
+$checksum              = '2868be0ec14568c8025c8a4da916af08479f54c683b9d504bf171fd086e7eaa9'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
