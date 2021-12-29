5676c228bea21a41dc7d64b1147153e06ff3bdb4
virtualex <info@ithinkvirtual.com>
2021-12-29 11:01:47 -0500

    [AU powerpanel-personal:2.3.4]

---
+++ automatic/powerpanel-personal/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://dl4jz3rbrsfum.cloudfront.net/software/PPP_Windows_v2.3.4.exe'
+$checksum               = '7838b776561275960f6e784bbfbf0da235e0e31191380d9b1150752188fff9a7'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "CyberPower PowerPanel Personal*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '-q'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
