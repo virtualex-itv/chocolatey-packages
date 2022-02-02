e3dec6f7d3e7d903f4b655ef8a62fdad0a11ce33
virtualex <info@ithinkvirtual.com>
2022-02-02 10:06:54 -0500

    (vmware-workstation-player) updated version check

--- automatic/vmware-workstation-player/update.ps1
+++ automatic/vmware-workstation-player/update.ps1
@@ -18,7 +18,7 @@ function CreateStream {
 
   $re = '\.exe$'
   $Url32 = "https://download3.vmware.com/software/player/file/" + ($jsonFile.downloadFiles | Where-Object fileName -match $re | Select-Object -First 1 -ExpandProperty fileName)
-  $version = $jsonFile.downloadFiles.version[0] + '.' + $jsonFile.downloadFiles.build[0]
+  $version = ($jsonFile.downloadFiles.version | Select-Object -First 1) + '.' + ($jsonFile.downloadFiles.build | Select-Object -First 1)
   $ChecksumType = 'sha256'
   $checksum = $jsonFile.downloadFiles.sha256checksum | Select-Object -First 1
   #endregion
