9c720dedc97a9ad415c00581bf1af4b5079a0b03
virtualex <virtualex-itv@users.noreply.github.com>
2022-02-02 15:08:50 +0000

    AU: 1 updated - vmware-workstation-player [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/d6b770e84dca2b63029be1dd2cb28b555f0b653f

--- automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
+++ automatic/vmware-workstation-player/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://download3.vmware.com/software/player/file/VMware-player-full-16.2.1-18811642.exe'
-$checksum               = 'a7eaa20f8a028a72a13d92e0fab48623e7b8aa1936e523306b9df20af5a4c7f3'
+$url                    = 'https://download3.vmware.com/software/player/file/VMware-player-full-16.2.2-19200509.exe'
+$checksum               = 'e309c9909f9b8b366153f282b1ea2c39d6ebafa0fea84079c1fac1b81cb5cbff'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
