02705ef9418dcc4746a67c92befe1225efe424c2
virtualex <virtualex-itv@users.noreply.github.com>
2022-02-06 12:02:05 +0000

    AU: 1 updated - rvtools [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/ceb974b4b9a442a63fdf72db3bc27e99c4d3647e

--- automatic/rvtools/tools/chocolateyinstall.ps1
+++ automatic/rvtools/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.2.2.msi'
-$checksum               = 'bb947325f9b58b715bcb5dd761ec8156b01d0fa07f4a0baeefa0ed75779bdc41'
+$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.3.1.msi'
+$checksum               = '9d5d8fde589f75912be9170ea6e286251e6635d523a3ff8d0a587097ec51a24a'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
