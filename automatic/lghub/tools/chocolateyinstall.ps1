ad60f33769531af83a3b2486a9bf9d54162c1c34
virtualex <virtualex-itv@users.noreply.github.com>
2022-03-01 16:24:10 +0000

    AU: 1 updated - lghub [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/94d2d071716ea915b812c6eba68318616a5a234d

--- automatic/lghub/tools/chocolateyinstall.ps1
+++ automatic/lghub/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = ''
-$checksum              = ''
-$checksumType          = ''
+$url                   = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe'
+$checksum              = 'EE7E56174B6B0A35C72D73F5B117E0DF040C43A9BD3574B64E884D49884676F1'
+$checksumType          = 'sha256'
 
 $packageArgs = @{
   packageName        = $env:ChocolateyPackageName
