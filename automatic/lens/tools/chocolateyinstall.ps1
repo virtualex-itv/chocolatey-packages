1f22fdaf2437a90946864e5d10268e089f22b773
virtualex <info@ithinkvirtual.com>
2022-03-16 07:49:15 -0400

    [AU lens:5.4.2]

--- automatic/lens/tools/chocolateyinstall.ps1
+++ automatic/lens/tools/chocolateyinstall.ps1
@@ -5,17 +5,38 @@ $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definiti
 $url                    = 'https://lens-binaries.s3-eu-west-1.amazonaws.com/ide/Lens%20Setup%205.4.2-latest.20220314.1.exe'
 $checksum               = '8c2c26a3ddccecf98918229692e666cf2d85ddbdffdf9c1e83c66fd71dbec0af'
 $checksumType           = 'sha256'
+$pp                     = Get-PackageParameters
 
-$packageArgs = @{
-  packageName           = $env:ChocolateyPackageName
-  unzipLocation         = $toolsDir
-  fileType              = 'exe'
-  softwareName          = 'Lens*'
-  url                   = $url
-  checksum              = $checksum
-  checksumType          = $checksumType
-  silentArgs            = '/currentuser /S'
-  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+if ( $pp.ALLUSERS) {
+
+  $packageArgs = @{
+    packageName           = $env:ChocolateyPackageName
+    unzipLocation         = $toolsDir
+    fileType              = 'exe'
+    softwareName          = 'Lens*'
+    url                   = $url
+    checksum              = $checksum
+    checksumType          = $checksumType
+    silentArgs            = '/allusers /disableAutoUpdates /S'
+    validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+  }
+
+  Install-ChocolateyPackage @packageArgs
+
+} else {
+
+  $packageArgs = @{
+    packageName           = $env:ChocolateyPackageName
+    unzipLocation         = $toolsDir
+    fileType              = 'exe'
+    softwareName          = 'Lens*'
+    url                   = $url
+    checksum              = $checksum
+    checksumType          = $checksumType
+    silentArgs            = '/currentuser /disableAutoUpdates /S'
+    validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+  }
+
+  Install-ChocolateyPackage @packageArgs
+
 }
-
-Install-ChocolateyPackage @packageArgs
