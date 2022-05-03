7740407b4c4b887eaf35a5166cd304b53f54a6d0
virtualex-itv <virtualex-itv@users.noreply.github.com>
2022-05-03 22:29:50 +0000

    AU: 1 updated - geekuninstaller [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/a906191aff91cbd6b37a33076690ee7b6952c4a8

--- automatic/geekuninstaller/tools/chocolateyinstall.ps1
+++ automatic/geekuninstaller/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = ''
-$checksum              = ''
-$checksumType          = ''
+$url                   = 'https://geekuninstaller.com/geek.zip'
+$checksum              = '1526c1201413f7868ff693b23debd41ee9ccf316ab8445887f3b5a8414a5d063'
+$checksumType          = 'sha256'
 
 $packageArgs = @{
   packageName          = $env:ChocolateyPackageName
