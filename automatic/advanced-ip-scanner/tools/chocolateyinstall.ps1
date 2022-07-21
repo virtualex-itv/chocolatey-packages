a2be686693686044da9fb821995a0d688cef3aa2
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-07-21 16:33:28 +0000

    AU: 1 updated - advanced-ip-scanner [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/ae225e4368a676eaccbb4884df04d328b10e4929

--- automatic/advanced-ip-scanner/tools/chocolateyinstall.ps1
+++ automatic/advanced-ip-scanner/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://download.advanced-ip-scanner.com/download/files/Advanced_IP_Scanner_2.5.4594.1.exe'
+$checksum               = '26d5748ffe6bd95e3fee6ce184d388a1a681006dc23a0f08d53c083c593c193b'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
