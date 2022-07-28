c245acc2a6f047f8e0569adcc90ee5a15f71f53d
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-07-28 12:03:04 +0000

    AU: 1 updated - lens [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/11a7e00186cbbb367c547418110bc783990dbefe

--- automatic/lens/tools/chocolateyinstall.ps1
+++ automatic/lens/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://downloads.k8slens.dev/ide/Lens%20Setup%205.5.4-latest.20220609.2.exe'
-$checksum               = 'c2365a9c4539f360179214e8d5f7591e81d1d4837b65c594dd32c2f93b772559'
+$url                    = 'https://downloads.k8slens.dev/ide/Lens%20Setup%206.0.0-latest.20220728.2.exe'
+$checksum               = '0d383e746b5f79e32651786f688d6575cadab3fb05361128419d50743dc49945'
 $checksumType           = 'sha256'
 $pp                     = Get-PackageParameters
 
