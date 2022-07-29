5f006fd2b39d4cc943aead0db89f5a9e07b40621
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-07-29 16:49:13 -0400

    (vmware-powercli-psmodule) update *.ps1

--- automatic/vmware-powercli-psmodule/tools/chocolateyinstall.ps1
+++ automatic/vmware-powercli-psmodule/tools/chocolateyinstall.ps1
@@ -2,7 +2,9 @@ $ErrorActionPreference = 'Stop'
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$packageName = 'vmware-powercli-psmodule'
+$pp           = Get-PackageParameters
+
+$packageName  = $env:ChocolateyPackageName
 $shortcutName = 'VMware.PowerCLI.lnk'
 $exe          = "$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
 $moduleName   = 'VMware.PowerCLI'
@@ -16,17 +18,36 @@ if ($PSversion -lt "5") {
   throw
 }
 
-if (Get-Module -ListAvailable -Name $moduleName -ErrorAction SilentlyContinue){
+$Provider = Get-PackageProvider -ListAvailable -ErrorAction SilentlyContinue
+if ( $Provider.Name -notmatch "NuGet" ) { 
+  Install-PackageProvider -Name NuGet -Confirm:$false -Force
+  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+} else {
+  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+
+if ( Get-Module -ListAvailable -Name "PowerShellGet" -ErrorAction SilentlyContinue ) {
+  Install-Module -Name "PowerShellGet" -AllowClobber -Force
+}
+
+if ( Get-Module -ListAvailable -Name $moduleName -ErrorAction SilentlyContinue ) {
   Write-Host "  ** Removing installed version, please be patient... **" -ForegroundColor Yellow
-  Get-InstalledModule -Name VMware.* | Uninstall-Module -AllVersions -Force -ErrorAction "SilentlyContinue" -Verbose
-  Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
   Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
+  Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+  Get-InstalledModule -Name "VMware.*" | Uninstall-Module -AllVersions -Force -ErrorAction SilentlyContinue
 }
 
 # Will fail if package version is a revised version not matching the module version, i.e. x.x.x.0020180101
 Write-Host "`n  ** Installing $moduleName v$moduleVers... **`n" -ForegroundColor Yellow
 Get-PackageProvider -Name NuGet -Force
-Install-Module -Name $moduleName -Scope AllUsers -RequiredVersion $moduleVers -AllowClobber -Force
 
-Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
-Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+if ($pp.ALLUSERS) {
+  Install-Module -Name $moduleName -Scope AllUsers -RequiredVersion $moduleVers -AllowClobber -Force
+} else {
+  Install-Module -Name $moduleName -Scope CurrentUser -RequiredVersion $moduleVers -AllowClobber -Force
+}
+
+if ( Get-Module -ListAvailable -Name $moduleName -ErrorAction SilentlyContinue ) {
+  Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+  Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+}
--- automatic/vmware-powercli-psmodule/tools/chocolateyuninstall.ps1
+++ automatic/vmware-powercli-psmodule/tools/chocolateyuninstall.ps1
@@ -2,7 +2,7 @@
 
 $shortcutName = 'VMware.PowerCLI.lnk'
 
-Get-InstalledModule -Name "VMware.*" |  Uninstall-Module -AllVersions -Force
-
-Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+
+Get-InstalledModule -Name "VMware.*" |  Uninstall-Module -AllVersions -Force
