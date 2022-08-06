297d46fa475e70d51f0403146911f27a3cb121bc
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 16:20:36 -0400

    (cursorfx) new manual package

---
+++ manual/cursorfx/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/f8276f56c973ad8a4a524340be1f8f925db362c1/icons/cursorfx.png" width="48" height="48"/> [cursorfx](https://community.chocolatey.org/packages/cursorfx)
+
+Customize your Windows cursor and create your own.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
---
+++ manual/cursorfx/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/cursorfx/CursorFX4_4.03_setup_sd.exe'
+$checksum               = '0198e05d4b4af04f82aef95e0c2d581ee4de15a454214ba200396be8d50581b5'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock CursorFX*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+$OSInfo = (Get-WmiObject Win32_OperatingSystem).ProductType
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock CursorFX requires an OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
