7f19b3836ec27d35634776addb02fb1bffcff980
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 15:30:41 -0400

    (object-desktop) new manual package

---
+++ manual/object-desktop/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/720b6025044a58df35e2c7da5b51ea3a4016e975/icons/object-desktop.png" width="48" height="48"/> [object-desktop](https://community.chocolatey.org/packages/object-desktop)
+
+The complete suite of Windows desktop enhancement products for customizing your PC experience.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ manual/object-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/objectdesktop/ObjectDesktop_setup_sd.exe'
+$checksum               = '7CD78BBBD441A00F09AD2007F94CB1FEC6F06D391A8662B9D1037F0027AA3562'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock Object Desktop*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
