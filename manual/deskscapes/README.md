0ea9e3f65cba10c211c5785b7802c0aeb085596d
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 17:26:31 -0400

    (deskscapes) new manual package

---
+++ manual/deskscapes/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/52e3ae0d90ab27ba52f42ec0fbe21422a069f745/icons/deskscapes.png" width="48" height="48"/> [deskscapes](https://community.chocolatey.org/packages/deskscapes)
+
+Personalize your desktop background with animated pictures and video.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
---
+++ manual/deskscapes/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/deskscapes/DeskScapes10_setup_sd.exe'
+$checksum               = 'E790E3D07BA153197314CDF5A14791A37DE96671F320F6A90BD6814D1C072A07'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock DeskScapes*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
