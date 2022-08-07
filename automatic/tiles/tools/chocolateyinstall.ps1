bd80904e1be21eadab96af0e03f3dbfa448731fd
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-08-07 12:50:09 +0000

    AU: 1 updated - tiles [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/08ad4d11dd8fee1e3d2b566ca33a1e7719b66752

--- automatic/tiles/tools/chocolateyinstall.ps1
+++ automatic/tiles/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://cdn.stardock.us/downloads/public/software/tiles/Tiles2_setup_sd.exe'
+$checksum               = '5bb497f723f8de2ca71fd111325bc97cc64a3b8898c4bd905de4b65e2f557b34'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
