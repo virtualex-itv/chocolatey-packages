a5b5935ca8c4cf445735d4cc2f12bc6154488882
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 23:03:40 -0400

    (shadowfx) New AU package

---
+++ automatic/shadowfx/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/9b59225e02150148b228647a569723a09becd12c/icons/shadowfx.png" width="48" height="48"/> [shadowfx](https://community.chocolatey.org/packages/shadowfx)
+
+Instantly add drop shadows to Windows 8 and 10.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/shadowfx/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock ShadowFX*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.2.9200'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock ShadowFX requires an OS running Windows 8 oh higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
