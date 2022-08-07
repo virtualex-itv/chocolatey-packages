2d9d1b704e7aaa05ccd7aae2ea1cda9416c39ff9
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 22:40:34 -0400

    (curtains) New AU Package

---
+++ automatic/curtains/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/de6fb2832128b844f0fd00dec9c7df96e0dae9fb/icons/curtains.png" width="48" height="48"/> [curtains](https://community.chocolatey.org/packages/curtains)
+
+Easily create and add additional style modes to Windows 10.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/curtains/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock Curtains*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='10.0.10240' #Windows 10 Threshold
+$max='10.0.20348' #Windows Server 2022 21H2
+
+If ( ( $OSVer -lt [version]$min ) -or ( $OSVer -ge [version]$max) ) {
+  Write-Warning "*** Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 10 or higher, but/or is not compatible with $((Get-WmiObject Win32_OperatingSystem).Caption)... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
