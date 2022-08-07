79cf92de02a0714d278aa22d62d8bc633a167e8b
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-06 23:26:31 -0400

    (start8) new AU package

---
+++ automatic/start8/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock Start8*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.2.9200'
+$max='6.3.9600'
+
+If ( ( $OSVer -lt [version]$min ) -or ( $OSVer -gt [version]$max ) ) {
+  Write-Warning "*** Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 8/8.1 or Windows Server 2012/2012 R2... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
---
+++ automatic/start8/update.ps1
@@ -0,0 +1,36 @@
+Import-Module AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+$history_page = 'https://www.stardock.com/products/start8/history'
+
+function global:au_GetLatest {
+  $releases = Invoke-WebRequest -Uri $history_page -UseBasicParsing
+
+  $Url = 'https://cdn.stardock.us/downloads/public/software/start/Start8_setup_sd.exe'
+  
+  $re = "Start8 (?<version>[\d\.]+[\d\.]+)"
+  $version = $releases -match $re | ForEach-Object { $Matches.version }
+  $ChecksumType = 'sha256'
+
+  @{
+    Url32             = $Url
+    Version           = $version
+    ChecksumType32    = $ChecksumType
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32 -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_SearchReplace {
+  @{
+      'tools\chocolateyInstall.ps1' = @{
+          "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+          "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+          "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      }
+  }
+}
+
+Update-Package -ChecksumFor none
