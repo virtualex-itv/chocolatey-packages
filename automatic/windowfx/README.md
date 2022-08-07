52205ed34d826268f656f87fc04219b7f352f876
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-07 09:41:55 -0400

    (windowfx) new AU package

---
+++ automatic/windowfx/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/b6e1be800dc6637d25fb124f2c24467b4273c4a5/icons/windowfx.png" width="48" height="48"/> [windowfx](https://community.chocolatey.org/packages/windowfx)
+
+Customize your windows and menus with stunning animations and effects.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/windowfx/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock WindowFX*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.0.6002'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock WindowFX requires an OS running Windows Vista or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
