a9d25bfa426416cea7210066ccff6828a4255250
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-08-07 13:44:29 +0000

    AU: 1 updated - windowfx [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/ec41ee4e6db806477bc35768ef4e088d45f2c2a4

--- automatic/windowfx/tools/chocolateyinstall.ps1
+++ automatic/windowfx/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://cdn.stardock.us/downloads/public/software/windowfx/WindowFX_setup_sd.exe'
+$checksum               = '73a633761f3248230f74f9681740d92b0cd4a6c68914823c5544447fd3e7c246'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
