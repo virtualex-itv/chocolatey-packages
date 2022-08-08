cb7d13bbc15c8d9a2cea0da466625b9336822822
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-07 21:11:34 -0400

    (stardock-multiplicity) new AU package

---
+++ automatic/stardock-multiplicity/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/39bf275cf5ef67cc91702260701dcbf9a835ae2d/icons/stardock-multiplicity.png" width="48" height="48"/> [stardock-multiplicity](https://community.chocolatey.org/packages/stardock-multiplicity)
+
+Control multiple PCs with one keyboard and mouse..
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/stardock-multiplicity/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock Multiplicity*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock Multiplicity requires an OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
