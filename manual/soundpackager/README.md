2d9b5496cabd9b84c50e401a58af5a3221a3fdb9
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-08 15:27:19 -0400

    (soundpackager) new manual package

---
+++ manual/soundpackager/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/b411e27a7798bfda4ea75ba6d631f0cca53ab532/icons/soundpackager.png" width="48" height="48"/> [soundpackager](https://community.chocolatey.org/packages/soundpackager)
+
+Customize your auditory experience by applying sound packages to your PC.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
---
+++ manual/soundpackager/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/soundpackager/SoundPackager10_setup_sd.exe'
+$checksum               = '5D17FA73B3397B9AF04C296535B6D39D8A23FD8D02C6BC90117E0CA521E66993'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock SoundPackager*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock SoundPackager requires a desktop OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
