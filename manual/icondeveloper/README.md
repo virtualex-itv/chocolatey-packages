a5b801ce25a58d17938758679faec2bf5f15d256
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-09 10:11:17 -0400

    (icondeveloper) new manual package

---
+++ manual/icondeveloper/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/e65f61320009fd9f522a9affd181a7a468c9cfa5/icons/icondeveloper.png" width="48" height="48"/> [icondeveloper](https://community.chocolatey.org/packages/icondeveloper)
+
+Create your own Windows icons from almost any graphic file.
+
+NOTE: `IconPackager` must also be installed to apply icon packages to the system but is not considered a dependency for this package.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
---
+++ manual/icondeveloper/tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/icondeveloper/IconDeveloper_setup_sd.exe'
+$checksum               = '4D477F194F63243D18FDEBC82A625D9D6BBC39F65B9A53E356F87F070C66A6DF'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock IconDeveloper*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+$OSProdType = (Get-WmiObject Win32_OperatingSystem).ProductType
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+$mid='6.2.9200'
+$max='10.0.19045'
+
+If ( $OSProdType -ne 1 ) {
+  Write-Warning "*** Stardock IconDeveloper requires a desktop OS running Windows 7/8.1/10... ***`n"
+  throw
+}
+
+If (( $OSVer -lt [version]$min ) -or ( $OSVer -eq [version]$mid ) -or ( $OSVer -gt [version]$max )) {
+  Write-Warning "*** Stardock IconDeveloper requires a desktop OS running Windows 7/8.1/10... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
