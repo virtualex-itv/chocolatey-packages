afd95fad752867a7521aec069262e92608af5a97
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-08 21:06:49 -0400

    (iconpackager) new AU package

---
+++ automatic/iconpackager/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/877660a895a3b1522a6105e5b8ccd07b032308dc/icons/iconpackager.png" width="48" height="48"/> [iconpackager](https://community.chocolatey.org/packages/iconpackager)
+
+Change all your Windows icons at once with custom icon packages.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/iconpackager/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock IconPackager*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock IconPackager requires a desktop OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
