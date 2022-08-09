c36ae9f3f4db8bc58dcf725fa239f1ab4d9f5315
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-09 17:14:44 -0400

    (windowblinds) new AU package

---
+++ automatic/windowblinds/README.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/a6a17ba116423e628f23a1febebb931ed230c670/icons/windowblinds.png" width="48" height="48"/> [windowblinds](https://community.chocolatey.org/packages/windowblinds)
+
+Customize the look and feel of your taskbar window frames and more.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/windowblinds/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock WindowBlinds*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+$max='10.0.19045'
+
+If (($OSVer -lt [version]$min) -or ($OSVer -gt [version]$max)) {
+  Write-Warning "*** Stardock WindowBlinds requires a desktop OS running Windows 7 or higher but/or is not compatible with $((Get-WmiObject Win32_OperatingSystem).Caption) ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
