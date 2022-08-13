2ee767f39fd62403ba29283cd136cc6d15f24fcc
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-12 21:03:26 -0400

    (jpegoptim) new AU package
    AU: 1 updated - jpegoptim [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/b943220c8f86bced5bfd575724e6421cb294f55f

---
+++ automatic/jpegoptim/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://github.com/XhmikosR/jpegoptim-windows/releases/download/1.4.7-rel1/jpegoptim-1.4.7-rel1-win64-msvc-2022-mozjpeg331-static-ltcg.zip'
+$checksum               = '204b671d4358c1445e70ebf5c50d819ddecb3eb85fd04c10abb07e6ac9fa075b'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "jpegoptim*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
