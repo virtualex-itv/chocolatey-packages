63afd403ff9ffc09a035c33d808ada6649b73dbe
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-08-13 01:07:17 +0000

    AU: 1 updated - jpegoptim [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/b943220c8f86bced5bfd575724e6421cb294f55f

--- automatic/jpegoptim/tools/chocolateyinstall.ps1
+++ automatic/jpegoptim/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://github.com/XhmikosR/jpegoptim-windows/releases/download/1.4.7-rel1/jpegoptim-1.4.7-rel1-win64-msvc-2022-mozjpeg331-static-ltcg.zip'
+$checksum               = '204b671d4358c1445e70ebf5c50d819ddecb3eb85fd04c10abb07e6ac9fa075b'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
