439699d7216437743cacc70d54882951cc7a1fd3
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-15 16:00:04 -0400

    (maint) update chocolateyInstall.ps1

--- automatic/laps/tools/chocolateyinstall.ps1
+++ automatic/laps/tools/chocolateyinstall.ps1
@@ -8,44 +8,30 @@ $checksumType        = 'sha256'
 $url64               = 'https://download.microsoft.com/download/C/7/A/C7AAD914-A8A6-4904-88A1-29E657445D03/LAPS.x64.msi'
 $checksum64          = '0001dd763cc74d37e3979e016ffcd0512d91494a0b3b7270c7a3bb4e1915f6d1'
 $checksumType64      = 'sha256'
+
 $pp                  = Get-PackageParameters
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  softwareName   = "Local Account Password Solution*"
+  url            = $url
+  url64bit       = $url64
+  validExitCodes = @(0, 3010)
+  silentArgs     = '/qn /norestart'
+  checksum       = $checksum
+  checksumType   = $checksumType
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
 
 if ( $pp.ALL ) {
   Write-Host "`nInstalling Microsoft Local Account Password Solution with Management Tools...`n" -ForegroundColor Yellow
 
-  $packageArgs = @{
-    packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
-    fileType       = 'msi'
-    softwareName   = "Local Account Password Solution*"
-    url            = $url
-    url64bit       = $url64
-    validExitCodes = @(0, 3010)
-    silentArgs     = 'ADDLOCAL=ALL /qn /norestart'
-    checksum       = $checksum
-    checksumType   = $checksumType
-    checksum64     = $checksum64
-    checksumType64 = $checksumType64
-  }
+  $packageArgs['silentArgs'] = 'ADDLOCAL=ALL /qn /norestart'
 
-  Install-ChocolateyPackage @packageArgs
 } else {
   Write-Host "`nInstalling Microsoft Local Account Password Solution...`n" -ForegroundColor Yellow
-
-  $packageArgs = @{
-    packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
-    fileType       = 'msi'
-    softwareName   = "Local Account Password Solution*"
-    url            = $url
-    url64bit       = $url64
-    validExitCodes = @(0, 3010)
-    silentArgs     = '/qn /norestart'
-    checksum       = $checksum
-    checksumType   = $checksumType
-    checksum64     = $checksum64
-    checksumType64 = $checksumType64
-  }
-
-  Install-ChocolateyPackage @packageArgs
 }
+
+Install-ChocolateyPackage @packageArgs
--- automatic/lens/tools/chocolateyinstall.ps1
+++ automatic/lens/tools/chocolateyinstall.ps1
@@ -7,36 +7,20 @@ $checksum               = '2da792098f2244f0f141a9df00f9ec5db11832de718beddae363f
 $checksumType           = 'sha256'
 $pp                     = Get-PackageParameters
 
-if ( $pp.ALLUSERS) {
-
-  $packageArgs = @{
-    packageName           = $env:ChocolateyPackageName
-    unzipLocation         = $toolsDir
-    fileType              = 'exe'
-    softwareName          = 'Lens*'
-    url                   = $url
-    checksum              = $checksum
-    checksumType          = $checksumType
-    silentArgs            = '/allusers /disableAutoUpdates /S'
-    validExitCodes        = @(0, 3010, 1605, 1614, 1641)
-  }
-
-  Install-ChocolateyPackage @packageArgs
-
-} else {
-
-  $packageArgs = @{
-    packageName           = $env:ChocolateyPackageName
-    unzipLocation         = $toolsDir
-    fileType              = 'exe'
-    softwareName          = 'Lens*'
-    url                   = $url
-    checksum              = $checksum
-    checksumType          = $checksumType
-    silentArgs            = '/currentuser /disableAutoUpdates /S'
-    validExitCodes        = @(0, 3010, 1605, 1614, 1641)
-  }
-
-  Install-ChocolateyPackage @packageArgs
-
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = 'Lens*'
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/currentuser /disableAutoUpdates /S'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
 }
+
+if ( $pp.ALLUSERS) {
+  $packageArgs['silentArgs'] = '/allusers /disableAutoUpdates /S'
+}
+
+Install-ChocolateyPackage @packageArgs
