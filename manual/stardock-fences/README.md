e5936307dfbafef18bd35f809338592ede9b7d7a
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-17 17:10:44 -0400

    (stardock-fences) new manual package

---
+++ manual/stardock-fences/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/9a54bd07ed3e744bbfe7176cf3eccfdb1a3c530b/icons/stardock-fences.png" width="48" height="48"/> [stardock-fences](https://community.chocolatey.org/packages/stardock-fences)
+
+Organize your PC by automatically placing your shortcuts and icons into resizable shaded areas on your desktop called fences that are designed to match Windows 10 and 11. Fences has many customization features that make it the world's most popular desktop enhancement.
+
+NOTE: This is commercial software, and only provides a trial version. Full usage will require the purchase of a license.
---
+++ manual/stardock-fences/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/fences/Fences3_setup_sd.exe'
+$checksum               = '92B3C835C025B0D4AB01CBF787D72DB1B591468774B6B9CD45B98820CBE0C6BF'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Stardock Fences*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+
+[version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
+$min='6.1.7601'
+
+If ( $OSVer -lt [version]$min ) {
+  Write-Warning "*** Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 7 or higher... ***`n"
+  throw
+} Else {
+  Install-ChocolateyPackage @packageArgs
+}
