205aa80253ba2542508a10a93a3b548284c58b3a
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-08-19 18:03:12 +0000

    AU: 1 updated - powerpanel-personal [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/ffc0c6821f8af68c9a3164623628530a2b9525de

--- automatic/powerpanel-personal/tools/chocolateyinstall.ps1
+++ automatic/powerpanel-personal/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://dl4jz3rbrsfum.cloudfront.net/software/PPP_Windows_v2.3.4.exe'
-$checksum               = '7838b776561275960f6e784bbfbf0da235e0e31191380d9b1150752188fff9a7'
+$url                    = 'https://dl4jz3rbrsfum.cloudfront.net/software/PPP_Windows_v2.4.6.exe'
+$checksum               = '5122c01046add97cd080700862d107a8b7e362357f6731ab81520e95ee6ddd84'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
