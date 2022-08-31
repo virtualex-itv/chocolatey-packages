29613c77ee3b0df3949f7617d8e3a202234c5973
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-31 08:29:38 -0400

    (stardock-fences) updated for v4.x

--- manual/stardock-fences/tools/chocolateyinstall.ps1
+++ manual/stardock-fences/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://cdn.stardock.us/downloads/public/software/fences/Fences3_setup_sd.exe'
-$checksum               = '92B3C835C025B0D4AB01CBF787D72DB1B591468774B6B9CD45B98820CBE0C6BF'
+$url                    = 'https://cdn.stardock.us/downloads/public/software/fences/Fences4_setup_sd.exe'
+$checksum               = '6F8AA84021CC190BFC9C7EFA375F6E8ADFAFA8BB1832BA5EA6DDC8A211CA888F'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
@@ -20,10 +20,10 @@ $packageArgs = @{
 
 
 [version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
-$min='6.1.7601'
+$min='10.0.10240'
 
 If ( $OSVer -lt [version]$min ) {
-  Write-Warning "*** Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 7 or higher... ***`n"
+  Write-Warning "*** This version of Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 10 or higher... ***`n"
   throw
 } Else {
   Install-ChocolateyPackage @packageArgs
