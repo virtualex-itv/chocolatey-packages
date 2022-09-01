8aea956e631f991c54c6eeb26d136b70ae60f925
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-08-31 20:40:37 -0400

    (spacemonger) new manual package

---
+++ manual/spacemonger/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://cdn.stardock.us/downloads/public/software/spacemonger/SpaceMonger_sd_setup.exe'
+$checksum               = '51A4478774EF998BAF8F54245AB3DD00A8AB1E2E7F8B7E334FE57A640A482F68'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "EdgeRunner SpaceMonger*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/s'
+  validExitCodes        = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
