09604b24f216d8f26ea716b759496626aacb5183
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-09-23 15:42:05 -0400

    (launchy) new manual package

---
+++ manual/launchy/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://launchy.net/downloads/win/Launchy2.5.exe'
+$checksum               = '4271E8F3969DC11259BC2791CE89B6EB73CB36EB663872792C0C4DFA87FA1916'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = "Launchy*"
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/VERYSILENT /NORESTART'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+}
+
+  Install-ChocolateyPackage @packageArgs
