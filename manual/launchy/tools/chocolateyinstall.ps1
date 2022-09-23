ce9f315f7f435360228e3a0758df9193d912689b
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-09-23 18:21:49 -0400

    (launchy) updated beta

--- manual/launchy/tools/chocolateyinstall.ps1
+++ manual/launchy/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://launchy.net/downloads/win/Launchy2.5.exe'
-$checksum               = '4271E8F3969DC11259BC2791CE89B6EB73CB36EB663872792C0C4DFA87FA1916'
+$url                    = 'https://launchy.net/downloads/win/LaunchySetup2.6B2.exe'
+$checksum               = '49E6E7F0FDD9BD16E30D827AC421BD9AFD5DD281577A9B08E8A0C3F91BAD62A1'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
