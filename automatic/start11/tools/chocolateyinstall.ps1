9ae45973820d4aee224b175d736649752f536cf4
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-11-01 13:22:41 -0400

    [AU start11:1.31]

--- automatic/start11/tools/chocolateyinstall.ps1
+++ automatic/start11/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference  = 'Stop';
+﻿$ErrorActionPreference  = 'Stop'
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
@@ -15,7 +15,7 @@ $packageArgs = @{
   checksum              = $checksum
   checksumType          = $checksumType
   silentArgs            = '/s'
-  validExitCodes        = @(0)
+  validExitCodes        = @(0, 9)
 }
 
 [version]$OSVer = (Get-WmiObject Win32_OperatingSystem).Version
@@ -24,6 +24,10 @@ $min='10.0.10240'
 If ( $OSVer -lt [version]$min ) {
   Write-Warning "*** Stardock $($packageName.substring(0,1).toupper()+$packageName.substring(1).tolower()) requires an OS running Windows 10 or 11... ***`n"
   throw
+}
+
+If (Test-Path ${env:ProgramFiles(x86)}"\Stardock\Start10\") {
+  Write-Warning "*** Stardock Start10 was found and will be uninstalled automagically before installation. ***`n"
 }
 
 Install-ChocolateyPackage @packageArgs
