210f34a9e03cb38ad9a6a50d9b580728dbb47b75
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-11-02 11:31:26 -0400

    (maint) bump history lines

--- update_all.ps1
+++ update_all.ps1
@@ -54,7 +54,7 @@ $Options = [ordered]@{
     }
 
     History = @{
-        Lines = 10                                         #Number of lines to show
+        Lines = 120                                         #Number of lines to show
         Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
         Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
     }
