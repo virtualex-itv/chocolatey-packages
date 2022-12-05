7233806755e814a1da371786915e177bb256063d
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-12-05 18:03:22 +0000

    AU: 1 updated - shellsend [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/96059a7ba730009569b274df963e98976010632e

--- automatic/shellsend/tools/chocolateyinstall.ps1
+++ automatic/shellsend/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/111/ShellSend-2.1.zip'
-$checksum              = '2ab484c88659dac991e47a5ba4316391a56c6139016904252a03c248c12459b3'
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/111/ShellSend-3.2.zip'
+$checksum              = 'af6f62a2a1eb15eda152e54a65c8de092e6729c74b31220e756ed445dda6ad13'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
