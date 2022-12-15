5b11e90a22c2b536b5f86e56df3bb470f43fc04b
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2022-12-15 18:05:34 +0000

    AU: 2 updated - lghub royalserver [skip ci] https://gist.github.com/113649984d5ef59e0ec2d529dfc28df4/32009a38ff019dbdd2a94e93e4bb8c4867d6a150

--- automatic/lghub/tools/chocolateyinstall.ps1
+++ automatic/lghub/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                   = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe'
-$checksum              = '112F29B7A6FD6A4DF3FACE4A73CDD207776295D2981B8FC273D68E4EB94714B2'
+$checksum              = '234BE8544A1CACDAADCCD0718D14BE477C861E38A1A8CF06EA5669EA5A472FB7'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
