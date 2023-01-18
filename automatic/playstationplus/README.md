7e1a7e750e85b5e808607f7e0cda44cf7ff96525
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-01-18 17:19:55 -0500

    (playstationplus) new AU package

---
+++ automatic/playstationplus/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/5b78fceafab72b8146f37a5db2407d85804becc8/icons/playstationplus.png" width="48" height="48"/> [playstationplus](https://community.chocolatey.org/packages/playstationplus)
+
+Enjoy hundreds of PS5, PS4 and classic PlayStation games, and unmissable benefits from three great value membership plans that unleash the power of your PlayStation console.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/playstationplus/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                   = ''
+$checksum              = ''
+$checksumType          = ''
+
+$packageArgs = @{
+  packageName        = $env:ChocolateyPackageName
+  unzipLocation      = $toolsDir
+  fileType           = 'exe'
+  softwareName       = "PlayStation Plus*"
+  url                = $url
+  checksum           = $checksum
+  checksumType       = $checksumType
+  silentArgs         = '/quiet'
+  validExitCodes     = @(0, 3010, 1641)
+}
+
+# operating system check
+$WindowsVersion=[Environment]::OSVersion.Version
+if ($WindowsVersion.Major -ne "10") {
+  throw "This package requires Windows 10 or Windows 11."
+}
+
+Install-ChocolateyPackage @packageArgs
