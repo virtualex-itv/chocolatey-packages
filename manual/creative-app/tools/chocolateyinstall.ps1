4dbc647747aeb08da32cb519364100943f8a4cf2
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-01-20 14:17:09 -0500

    (creative-app) new manual package

---
+++ manual/creative-app/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference  = 'Stop'
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://files.creative.com/creative/bin/apps/swureleases/win/creativeapp/release/CreativeAppSetup_1.12.03.00.exe'
+$checksum               = '7B01EE81A712F13071B27C0BDFB36402F0AEDC7D193B81853512569C82FB269E'
+$checksumType           = 'sha256'
+$pp                     = Get-PackageParameters
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = 'Creative App*'
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/SP- /VERYSILENT /NORESTART'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
