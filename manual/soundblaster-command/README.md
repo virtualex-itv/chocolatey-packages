de18d7f8dc62552bd98f8eb2bd12ea3ccb598f8c
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-01-20 12:49:44 -0500

    (soundblaster-command) new manual package

---
+++ manual/soundblaster-command/README.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/f17bb6121b170badbed892c08bc05d99871470fe/icons/soundblaster-command.png" width="48" height="48"/> [soundblaster-command](https://community.chocolatey.org/packages/soundblaster-command)
+
+Available for Windows®, the comprehensive and easy-to-use Sound Blaster Command software offers users a complete control over their istening experience from audio realism to immersive audio effects, all which will intelligently embellish how users perceive sounds.
---
+++ manual/soundblaster-command/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference  = 'Stop'
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://files.creative.com/creative/bin/apps/swureleases/win/sbcommand/release/CreativeSBCommandSetup_3.5.09.00.exe'
+$checksum               = '1B49F9E8C34E90322CA4EE950AA10144F681A81E9C979A3030F8032629A06EF8'
+$checksumType           = 'sha256'
+$pp                     = Get-PackageParameters
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = 'Sound Blaster Command*'
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/SP- /VERYSILENT /NORESTART'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
