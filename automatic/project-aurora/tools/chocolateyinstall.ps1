5e0ee91b6c6050f12439a47db07c0faa1ee97b39
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-01-27 09:20:32 -0500

    (project-aurora) new AU package

---
+++ automatic/project-aurora/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference  = 'Stop'
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = ''
+$checksum               = ''
+$checksumType           = ''
+$pp                     = Get-PackageParameters
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = 'Aurora*'
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/SP- /VERYSILENT /NORESTART'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
