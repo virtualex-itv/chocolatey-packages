e556a374ebadb8ab70d20296821b00fec6dc83a0
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-02-16 17:39:38 -0500

    (logioptionsplus) update uninstall cmd

--- automatic/logioptionsplus/tools/chocolateyuninstall.ps1
+++ automatic/logioptionsplus/tools/chocolateyuninstall.ps1
@@ -4,4 +4,6 @@ $softwareName = 'Logi Options+'
 
 [array]$key = Get-UninstallRegistryKey -softwareName "$($softwareName)"
 
-& cmd /c $key.QuietUninstallString
+$cmd = "/c ${key.QuietUninstallString}"
+
+Start-ChocolateyProcessAsAdmin -Statements $cmd -ExeToRun "cmd.exe"
