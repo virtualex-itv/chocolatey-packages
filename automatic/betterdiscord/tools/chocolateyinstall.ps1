6336398d089a17de21ecd7810732d9c996401d4e
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-02-22 20:40:37 -0500

    [AU betterdiscord:1.2.0]

--- automatic/betterdiscord/tools/chocolateyinstall.ps1
+++ automatic/betterdiscord/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = ''
-$checksum               = ''
-$checksumType           = ''
+$url                    = 'https://github.com/BetterDiscord/Installer/releases/download/v1.2.0/BetterDiscord-Windows.exe'
+$checksum               = '62101b7f23db65cdeb32e572b25631aee3e47c41949846c0f9ad3697b86cabbe'
+$checksumType           = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
