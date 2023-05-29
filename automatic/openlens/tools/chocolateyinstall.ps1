c3d136aa602764f60e3fd22dc160b66910c65c13
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2023-05-29 18:03:00 +0000

    AU: 1 updated - openlens [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/be83351515cd572478b3922da97be3661481fc40

--- automatic/openlens/tools/chocolateyinstall.ps1
+++ automatic/openlens/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.4.15/OpenLens-6.4.15.exe'
-$checksum               = '863dba481ff36140e0ce575fc1ee941436b0cbf36f259b8dc5fbe838ba686f9f'
+$url                    = 'https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.0/OpenLens-6.5.0.exe'
+$checksum               = 'ae3c716e2fe57cc5ea63449223b55dd27473ffbdd97cfa54e9ed7894ad8a005d'
 $checksumType           = 'sha256'
 $pp                     = Get-PackageParameters
 
