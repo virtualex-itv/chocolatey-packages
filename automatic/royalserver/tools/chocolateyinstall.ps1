01da089c7bbba08c27ff5cc721353a1b99b5d111
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2023-06-23 12:03:43 +0000

    AU: 2 updated - royalserver rvtools [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/35b9abdc521f986518c24e6970fa55da2bd4ab50

--- automatic/royalserver/tools/chocolateyinstall.ps1
+++ automatic/royalserver/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_4.01.60416.0.msi'
-$checksum              = 'f48b7f52c7ccd5d038fc65770d346b287c29ab92194ca23d7ce9ca84b3947976'
+$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_4.01.60622.0.msi'
+$checksum              = '0cd1d31626312bd54afd3ba5a8ca5c7aeecefc2a8fe1fcddb836d8eeb8f92f72'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
--- automatic/rvtools/tools/chocolateyinstall.ps1
+++ automatic/rvtools/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.4.2.msi'
-$checksum               = '19cdc83c7eac3e6697792b2a4ebef3e3d0dd15053dfdc82ed5fb1b68197f1000'
+$url                    = 'https://robware.net/download/c2f0942d59a42901b18e06ea73a4dc4be8e57670c8973429c5ba5da99f60580412/RVTools4.4.3.msi'
+$checksum               = 'a2bcba534c64335c267c760f308895cc12ab75b1c7188be77c189daa84c514a2'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
