91309299c1198a618322f037524e0851b518cbea
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-08-05 07:47:48 -0400

    (project-aurora) rem unused var

--- automatic/project-aurora/tools/chocolateyinstall.ps1
+++ automatic/project-aurora/tools/chocolateyinstall.ps1
@@ -5,7 +5,6 @@ $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definiti
 $url                    = 'https://github.com/Aurora-RGB/Aurora/releases/download/v145/Aurora-setup-v145.exe'
 $checksum               = '91a306c1485d9e55f42071985339dda4a0d411db98707af6b640dd5ed55a3c2d'
 $checksumType           = 'sha256'
-$pp                     = Get-PackageParameters
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
