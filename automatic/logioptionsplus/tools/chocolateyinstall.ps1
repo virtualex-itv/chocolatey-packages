0820c921a4027c478c4a168626e6f0f3430a9dde
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2023-09-15 06:03:01 +0000

    AU: 1 updated - logioptionsplus [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/732f7d781e8a7f5ce82d3274bfeb750a09c398c6

--- automatic/logioptionsplus/tools/chocolateyinstall.ps1
+++ automatic/logioptionsplus/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                   = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
-$checksum              = 'E12F3F7C3458747B3D24E3D5B9B696D48A043C11195F42E37256DF8953D88EAD'
+$checksum              = '3E283824B1A3C66ED788E9427BC8556174A894D14EFAB82FA19955C289E339BC'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
