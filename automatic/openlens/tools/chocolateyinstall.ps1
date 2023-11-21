9bab0edd88b0f8f5e121e55a6c5203c86e2e6cca
jackmtpt <115712715+jackmtpt@users.noreply.github.com>
2023-11-21 15:17:52 +0000

    Fix openlens package so that it downloads the installer, not the standalone exe

--- automatic/openlens/tools/chocolateyinstall.ps1
+++ automatic/openlens/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.2-366/OpenLens.6.5.2-366.exe'
-$checksum               = '8502c1dc2071f3a0964b4d0faf41adc999af17fc68baacf38f39e2ba38e6d99c'
+$url                    = 'https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.2-366/OpenLens.Setup.6.5.2-366.exe'
+$checksum               = '9138293633CB9447041D7319619B1D59999508A42E65F024780540F7ED27CEE8'
 $checksumType           = 'sha256'
 $pp                     = Get-PackageParameters
 
