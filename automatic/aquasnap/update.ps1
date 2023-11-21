b660cadd18bcffade449b3fc8d398fc50518b46e
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2023-11-21 13:29:39 -0500

    (aquasnap) add param

--- automatic/aquasnap/update.ps1
+++ automatic/aquasnap/update.ps1
@@ -4,7 +4,7 @@ Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 $releases = 'https://www.nurgo-software.com/pricing/aquasnap'
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases #-UseBasicParsing #(causes 403 error in PS 5.x)
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $re = '.msi'
   $url = $download_page.links | Where-Object { $_.href -match $re } | Select-Object -First 1 -ExpandProperty href
@@ -12,12 +12,12 @@ function global:au_GetLatest {
   $ChecksumType = 'sha256'
 
   $versUrl = 'https://www.nurgo-software.com/company/news/13-aquasnap'
-  $versPage = Invoke-WebRequest -Uri $versUrl #-UseBasicParsing #(causes 403 error in PS 5.x)
-  
+  $versPage = Invoke-WebRequest -Uri $versUrl -UseBasicParsing
+
   $re = "AquaSnap v(?<version>[\d\.]+)"
   $versPage.Content -match $re
   $version = $matches.version
-  
+
   @{
     Url32             = $Url32
     Version           = $version
