54ef2b5198ce5b17572f749896f29d5b9ea0f29d
Alex López <info@l0p3z.net>
2023-12-02 08:35:38 -0500

    fix(openlens): Update chocolateyinstall.ps1

--- automatic/openlens/tools/chocolateyinstall.ps1
+++ automatic/openlens/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://github.com/MuhammedKalkan/OpenLens/releases/download/v6.5.2-366/OpenLens.Setup.6.5.2-366.exe'
-$checksum               = '9138293633CB9447041D7319619B1D59999508A42E65F024780540F7ED27CEE8'
+$checksum               = '9138293633cb9447041d7319619b1d59999508a42e65f024780540f7ed27cee8'
 $checksumType           = 'sha256'
 $pp                     = Get-PackageParameters
 
