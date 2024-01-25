94756e34bcb61209048a2c8694d1c8ef23349a39
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2024-01-25 18:08:13 +0000

    AU: 1 updated - capture [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/26fba4446615e2f10deee641602a2d3b66fe4299

--- automatic/capture/tools/chocolateyinstall.ps1
+++ automatic/capture/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://cdn.cloud.techsmith.com/techsmithcapture/win/x64/Setup.exe'
-$checksum               = 'B66B5690AD4F839F5C4AC14217B48A40CF42E9C27DC6FBE3C975356DF826F666'
+$checksum               = '006CEAEA8820C4E98ED6EDE027543A5E15D4B64399037EE29C48BA3A466F3E76'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
