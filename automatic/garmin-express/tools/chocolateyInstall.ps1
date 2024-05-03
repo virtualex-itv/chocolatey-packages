02cefec96e9d3597e0bd282f5f69e62de4db0b14
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2024-05-03 06:04:46 +0000

    AU: 1 updated - garmin-express [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/391e02bb3e525ec681fb44deb2d023d9f63315ab

--- automatic/garmin-express/tools/chocolateyInstall.ps1
+++ automatic/garmin-express/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType      = "EXE"
   url           = "https://download.garmin.com/omt/express/GarminExpress.exe"
 
-  checksum      = 'b0493108925ef3a043ece597977ab559549f318a4cae507a1795c94a2c685efa'
+  checksum      = '8f57de7826a5990fe360c4112924deac532482e046a288f15d539957f6100344'
 
   checksumType  = 'sha256'
   silentArgs    = '/quiet /norestart'
