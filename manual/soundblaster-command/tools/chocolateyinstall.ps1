db0f313a9927dc188f9a24ab522aa1705a4210c2
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2024-05-16 14:52:35 -0400

    chore(soundblaster-command): update version

--- manual/soundblaster-command/tools/chocolateyinstall.ps1
+++ manual/soundblaster-command/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://files.creative.com/creative/bin/apps/swureleases/win/sbcommand/release/CreativeSBCommandSetup_3.5.09.00.exe'
-$checksum               = '1B49F9E8C34E90322CA4EE950AA10144F681A81E9C979A3030F8032629A06EF8'
+$url                    = 'https://files.creative.com/creative/bin/apps/swureleases/win/sbcommand/release/CreativeSBCommandSetup_3.5.10.00.exe'
+$checksum               = '2b6d9bcd9f0a436c90a34e3af5ee3706c3e749cbe703ebddccfad9c21747a722'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
