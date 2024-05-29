f5965d7420d086f6ecc82a01a2d266383de61779
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2024-05-29 12:44:27 -0400

    chore(rvtools): minor revisions

--- automatic/rvtools/update.ps1
+++ automatic/rvtools/update.ps1
@@ -1,18 +1,22 @@
 Import-Module Chocolatey-AU
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
-$baseURL = "https://fdendpoint-tf-ae40cce298ca76ff-prod-a2bhbrbte7exh5d4.a01.azurefd.net/resources/prod/"
-$jsFile = "$($baseURL)versionInfo.json"
+$baseURL = "https://resources.robware.net/resources/prod/"
+$jsFile = "$($baseURL)versions_manifest.json"
 
 function global:au_GetLatest {
     $jsContent = (Invoke-WebRequest -Uri $jsFile).Content | ConvertFrom-Json
 
-    $version = $jsContent.version[0]
-    $Url32 = "${baseURL}RVTools${version}.msi"
+    $version = $jsContent.version
+    $Url32 = "$baseURL$($jsContent.Name)"
+    $checksum = $jsContent.SHA256
+    $checksumType = "sha256"
 
     @{
       Version           = $version
       Url32             = $Url32
+      Checksum32        = $checksum
+      ChecksumType32    = $checksumType
     }
 }
 
@@ -26,4 +30,4 @@ function global:au_SearchReplace {
   }
 }
 
-Update-Package -ChecksumFor 32
+Update-Package -ChecksumFor none
