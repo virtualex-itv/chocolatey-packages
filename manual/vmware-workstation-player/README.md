bc9d68b6bbace376cb65a78d5f223c77f384fd50
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2024-09-18 20:30:19 -0400

    chore(vmware-workstation-player): updated

---
+++ manual/vmware-workstation-player/README.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/b6e6a6262927b9decf4a7702d926e0df8ecea61d/icons/vmware-workstation-player.png" width="48" height="48"/> [vmware-workstation-player](https://community.chocolatey.org/packages/vmware-workstation-player)
+
+VMware Workstation Player™ is a streamlined desktop virtualization application that runs another operating system on the same computer without rebooting. VMware Workstation Player provides a simple user interface, unmatched operating system support, and portability across the VMware ecosystem.
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ manual/vmware-workstation-player/tools/chocolateyInstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference  = 'Stop'
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://softwareupdate.vmware.com/cds/vmw-desktop/player/17.5.2/23775571/windows/core/VMware-player-17.5.2-23775571.exe.tar'
+$checksum               = 'f4418ae1a9c1c4a1092519efb107601d16aec8277618eca393a0c2e07b80782f'
+$checksumType           = 'sha256'
+
+$zippackageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+
+}
+
+Install-ChocolateyZipPackage @zippackageArgs
+
+$fileLocation = Get-ChildItem $toolsDir\VMware-player*.exe
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  file                  = $fileLocation
+  softwareName          = 'VMware Player*'
+  silentArgs            = '/s /v /qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 REBOOT=ReallySuppress'
+  validExitCodes        = @(0, 3010, 1614, 1641)
+
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ErrorAction SilentlyContinue; if (Test-Path $_) { Set-Content "$_.ignore" } }
