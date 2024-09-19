59f797a3a3dc4e55b64095c9cc8cc1b41c41abc8
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2024-09-18 20:29:19 -0400

    chore(vmware-tools): updated

---
+++ manual/vmware-tools/README.md
@@ -0,0 +1,24 @@
+# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/6ce9dc8f51e1b0adfcd4a2d620988b33caf1ccb3/icons/vmware-tools.png" width="48" height="48"/> [vmware-tools](https://community.chocolatey.org/packages/vmware-tools)
+
+VMware Tools is a set of services and modules that enable several features in VMware products for better management of, and seamless user interactions with, guests operating systems.
+
+---
+
+## Package Parameters
+
+* `/ALL` - Performs a Complete Install of VMware Tools (Optional)
+Example:
+
+```shell
+choco install vmware-tools --params='"/ALL"'
+```
+
+---
+
+## [choco://vmware-tools](choco://vmware-tools)
+
+To use choco:// protocol URLs, install [(unofficial) choco:// Protocol support](https://chocolatey.org/packages/choco-protocol-support)
+
+---
+
+**Please Note**: This is an automatically updated package. If you find it is out of date by more than a day or two, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ manual/vmware-tools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                   = 'https://packages.vmware.com/tools/releases/12.4.5/windows/x86/VMware-tools-12.4.5-23787635-i386.exe'
+$checksum              = '5229bbf3d3c8bf4a84e04eb8a2cfc675d48257034e17346e21af218fb7285c19'
+$ChecksumType          = 'sha256'
+$url64                 = 'https://packages.vmware.com/tools/releases/12.4.5/windows/x64/VMware-tools-12.4.5-23787635-x86_64.exe'
+$checksum64            = '0d277be897d95db40998e626883a77df564257134164a9cccc666d6588c01c33'
+$ChecksumType64        = 'sha256'
+
+$pp                    = Get-PackageParameters
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  url64bit       = $url64
+  validExitCodes = @(0, 3010)
+  silentArgs     = '/S /v /qn REBOOT=R'
+  softwareName   = "VMware Tools*"
+  checksum       = $checksum
+  checksumType   = $ChecksumType
+  checksum64     = $checksum64
+  checksumType64 = $ChecksumType64
+}
+
+if ( $pp.ALL ) {
+  Write-Host "`nPerforming a Complete installation of VMware Tools...`n" -ForegroundColor Yellow
+
+  $packageArgs['silentArgs'] = '/S /v /qn REBOOT=R ADDLOCAL=ALL'
+
+} else {
+  Write-Host "`nPerforming a Typical installation of VMware Tools...`n" -ForegroundColor Yellow
+}
+
+Install-ChocolateyPackage @packageArgs
