614c3bc3aa2bc0dd9d4416ef586e4d77b40e4858
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2024-09-21 17:49:19 -0400

    revert(vmware-tools): revert commit

--- automatic/vmware-tools/tools/ChocolateyInstall.ps1
+++ automatic/vmware-tools/tools/ChocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://packages.vmware.com/tools/releases/10.3.10/windows/x86/VMware-tools-10.3.10-12406962-i386.exe'
-$checksum              = '59a498cc1641a04c10d08709ac6dc6aa95cafc7b56e6d613d3630e89083f0f4c'
+$url                   = 'https://packages.vmware.com/tools/releases/12.4.5/windows/x86/VMware-tools-12.4.5-23787635-i386.exe'
+$checksum              = '5229bbf3d3c8bf4a84e04eb8a2cfc675d48257034e17346e21af218fb7285c19'
 $ChecksumType          = 'sha256'
-$url64                 = 'https://packages.vmware.com/tools/releases/10.3.10/windows/x64/VMware-tools-10.3.10-12406962-x86_64.exe'
-$checksum64            = '65d5cc22d2fae73f104e985baa7885a1544adceb0774cc302522c03541e5dd82'
+$url64                 = 'https://packages.vmware.com/tools/releases/12.4.5/windows/x64/VMware-tools-12.4.5-23787635-x86_64.exe'
+$checksum64            = '0d277be897d95db40998e626883a77df564257134164a9cccc666d6588c01c33'
 $ChecksumType64        = 'sha256'
 
 $pp                    = Get-PackageParameters
