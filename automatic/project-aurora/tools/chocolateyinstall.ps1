a70ca4ec2bf9a3567fb87a6005c96af66db77ba2
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2024-10-01 00:07:05 +0000

    AU: 1 updated - project-aurora [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/83cc04106888663e29f34e6680e7049bc313c8ff

--- automatic/project-aurora/tools/chocolateyinstall.ps1
+++ automatic/project-aurora/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://github.com/Aurora-RGB/Aurora/releases/download/v291/Aurora-setup-v291.exe'
-$checksum               = '6e9de5567cb92656b6eebbb7ff9433b02160a277a41421adf09b414bb854000c'
+$url                    = 'https://github.com/Aurora-RGB/Aurora/releases/download/v292/Aurora-setup-v292.exe'
+$checksum               = '45a980ef83afc01ab58f75eb405976d1da29fc1d926157c3f1a61231faa290c9'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
