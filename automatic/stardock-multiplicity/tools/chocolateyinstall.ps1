09ba7bfc20115e554b2f36f321d4a2c2b48c85c2
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2024-10-08 18:03:55 +0000

    AU: 1 updated - stardock-multiplicity [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/7a784a79c4dd4d5834c6fc8ded9122134193956b

--- automatic/stardock-multiplicity/tools/chocolateyinstall.ps1
+++ automatic/stardock-multiplicity/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://cdn.stardock.us/downloads/public/software/multiplicity/Multiplicity3_setup_sd.exe'
-$checksum               = 'e9fdf63506a606f0ef304b50700e5fce95a85b8aa1659118945aff419dc825d5'
+$checksum               = '82bd35e7e69f74da497e4b0fa7f73d4b7089f7c6f9deb994c798dc01b0a78d54'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
