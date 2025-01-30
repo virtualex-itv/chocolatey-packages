5959b0c170ceda30d35841f5dda33f8e2f49fa47
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-01-29 20:05:21 -0500

    chore(vmware-horizon-client): restore contents

---
+++ automatic/vmware-horizon-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://download3.omnissa.com/software/CART25FQ4_WIN_2412/Omnissa-Horizon-Client-2412-8.14.0-12437220870.exe'
+$checksum               = '65449c9a7827c9ddbff858706e57aae53ac61cf07b6321d1b57c037575b5532f'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  url                   = $url
+  softwareName          = "Omnissa Horizon Client*"
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/silent /norestart'
+  validExitCodes        = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
