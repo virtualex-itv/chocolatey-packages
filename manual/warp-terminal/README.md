d52ad4fd91555042390f78176dd0a5da54b82870
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-02-27 14:40:30 -0500

    feat(warp-terminal): new manual package

---
+++ manual/warp-terminal/README.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://rawcdn.githack.com/virtualex-itv/chocolatey-packages/40d037a72e4d24dae1a1ae1ef2aa45925b180c3d/icons/warp-terminal.png" width="48" height="48"/> [warp-terminal](https://community.chocolatey.org/packages/warp-terminal)
+
+Become a command line power user on day one. Warp combines AI and your dev team's knowledge in one fast, intuitive terminal.
---
+++ manual/warp-terminal/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url                    = 'https://releases.warp.dev/stable/v0.2025.02.24.20.50.stable_04/WarpSetup.exe'
+$checksum               = '83e39bb4b1f9d627285d9f69d04ad3a07f725ca51e2be569b9a63b1b8f16774c'
+$checksumType           = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'exe'
+  softwareName          = 'Warp*'
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+  silentArgs            = '/ALLUSERS=0 /VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
+  validExitCodes        = @(0, 3010, 1605, 1614, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
