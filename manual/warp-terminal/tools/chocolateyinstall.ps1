47645397d425885cbc5fa99dfa53121b2259bff4
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-05-03 15:41:37 -0400

    chore(warp-terminal): package update

--- manual/warp-terminal/tools/chocolateyinstall.ps1
+++ manual/warp-terminal/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://releases.warp.dev/stable/v0.2025.04.16.08.11.stable_02/WarpSetup.exe'
-$checksum               = 'c9a47c216c8e6d8fa6253956955e291c2251efb2acc8db4e1e3173c7d9813e5e'
+$url                    = 'https://releases.warp.dev/stable/v0.2025.04.23.08.11.stable_01/WarpSetup.exe'
+$checksum               = '3231fc2f4ee66cbc0e0eb69740d3375eac7f942f34c9827f9d3aeb1fc3eec62e'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
