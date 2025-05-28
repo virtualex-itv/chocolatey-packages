1a3b89a8af531b7078053080a4de84a39cc11340
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-05-28 10:20:01 -0400

    fix(start11): update for v2 detection

--- automatic/start11/update.ps1
+++ automatic/start11/update.ps1
@@ -5,11 +5,19 @@ $history_page = 'https://www.stardock.com/products/start11/history'
 
 function global:au_GetLatest {
   $releases = Invoke-WebRequest -Uri $history_page -UseBasicParsing
+  $content = $releases.Content
 
-  $Url = 'https://cdn.stardock.us/downloads/public/software/start/Start11_Setup.exe'
+  $Url = 'https://cdn.stardock.us/downloads/public/software/start/v2/Start11v2-setup.exe'
 
-  $re = "Start11 (?<version>[\d\.]+[\d\.]+)"
-  $version = $releases -match $re | ForEach-Object { $Matches.version }
+  $pattern = 'Start11 (?<version>\d+(\.\d+)+)(?<beta> Beta)?'
+  $matches = [regex]::Matches($content, $pattern)
+  $version = $null
+  foreach ($m in $matches) {
+    if (-not $m.Groups['beta'].Success) {
+        $version = $m.Groups['version'].Value
+        break
+    }
+  }
   $ChecksumType = 'sha256'
 
   @{
