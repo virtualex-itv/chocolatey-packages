2b45f81b6c6d7e944916fe2626bef60161b82403
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-07-24 16:52:41 -0400

    chore(rvtools): update for revised urls

--- automatic/rvtools/tools/chocolateyinstall.ps1
+++ automatic/rvtools/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://resources.robware.net/resources/prod/RVTools4.7.1.msi'
+$url                    = 'https://downloads.dell.com/rvtools/rvtools4.7.1.msi'
 $checksum               = '0506126bcbc4641d41c138e88d9ea9f10fb65f1eeab3bff90ad25330108b324c'
 $checksumType           = 'sha256'
 
@@ -18,11 +18,22 @@ $packageArgs = @{
   validExitCodes        = @(0)
 }
 
-$isInstalled = Get-CimInstance -ClassName Win32_Product | Where-Object { $_.Name -like "*$env:ChocolateyPackageName*" }
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($isInstalled) {
-  Write-Host "`n$env:ChocolateyPackageName installation found, uninstalling prior to upgrade...`n" -ForegroundColor Yellow
-  $isInstalled | Invoke-CimMethod -MethodName Uninstall | Out-Null
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+
+      $packageArgs['file'] = ''
+    }
+
+    Write-Host "`n$env:ChocolateyPackageName installation found, uninstalling prior to upgrade...`n" -ForegroundColor Yellow
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} else {
+  Write-Host "`nNo previous installation of $env:ChocolateyPackageName found, proceeding with installation...`n" -ForegroundColor Green
 }
 
 Install-ChocolateyPackage @packageArgs
