deb8f6518178773bfe1b256b0bb80949366698c3
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-10-21 15:11:38 -0400

    chore(omnissa-horizon-client): manual update to 8.16.0

--- automatic/omnissa-horizon-client/tools/chocolateyinstall.ps1
+++ automatic/omnissa-horizon-client/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://download3.omnissa.com/software/CART26FQ1_WIN_2503/Omnissa-Horizon-Client-2503-8.15.0-14236595709.exe'
-$checksum               = 'ac3d0faf500b0e87ab6b51f143c2aaea03412bebf7da91528cb68b4d0576c3b2'
+$url                    = 'https://download3.omnissa.com/software/CART26FQ2_WIN_2506/Omnissa-Horizon-Client-2506-8.16.0-16560451995.exe'
+$checksum               = 'ca5887abae262c4078fcfe60657c35f12a87c3506bf2e9236d00a8f0b9d7f5ef'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
