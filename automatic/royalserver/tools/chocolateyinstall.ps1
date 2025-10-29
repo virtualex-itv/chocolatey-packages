85e78b0003f8526f1f564172d157ef2c23f4f642
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2025-10-29 12:11:54 +0000

    AU: 1 updated - royalserver [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/20740ad985480b27eb84155cb230a155b15d3047

--- automatic/royalserver/tools/chocolateyinstall.ps1
+++ automatic/royalserver/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_5.03.51001.0.msi'
-$checksum              = '0ec557cb5414159830ab6040f178c5964b37e72ae3ffe62151de04764e66bca8'
+$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_5.03.51029.0.msi'
+$checksum              = '12881ebc1b7ed038083a3b4b7750a8ec7c0be638b44d46af3c6a9b6308ba5f8a'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
