34421aee356e61b11e5e00752ef51d01efddd22d
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-01-20 00:12:26 +0000

    AU: 1 updated - termius [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/e5915afbe39de06862210ae453e3e78db2ea956c

--- automatic/termius/tools/chocolateyinstall.ps1
+++ automatic/termius/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://autoupdate.termius.com/windows/Install%20Termius.exe'
-$checksum               = '39D96DFE2B13E842F210D17FADC4A359CFFB70FC5775F6BAFAC654AC37107ADF'
+$checksum               = 'D98B289A6D623DE5ED5CDE97A8D65F4A84431E06F30758621D3A2A1083CE8D6C'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
