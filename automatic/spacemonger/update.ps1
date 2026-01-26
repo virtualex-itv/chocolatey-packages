921838061731516853ecba002d3b29bcd8a77226
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2026-01-26 08:50:28 -0500

    chore(spacemonger): convert from manual to AU package

---
+++ automatic/spacemonger/update.ps1
@@ -0,0 +1,38 @@
+Import-Module Chocolatey-AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+$history_page = 'https://forums.stardock.com/499121/spacemonger-history'
+
+function global:au_GetLatest {
+  $releases = Invoke-WebRequest -Uri $history_page -UseBasicParsing
+
+  $Url = 'https://cdn.stardock.us/downloads/public/software/spacemonger/SpaceMonger_sd_setup.exe'
+
+  # Match versions like "SpaceMonger 3.0"
+  $re = "SpaceMonger (?<version>\d+\.\d+(?:\.\d+)*)"
+  $null = $releases.Content -match $re
+  $version = $Matches.version
+  $ChecksumType = 'sha256'
+
+  @{
+    Url32             = $Url
+    Version           = $version
+    ChecksumType32    = $ChecksumType
+  }
+}
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32 -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_SearchReplace {
+  @{
+      'tools\chocolateyInstall.ps1' = @{
+          "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+          "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+          "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      }
+  }
+}
+
+Update-Package -ChecksumFor none
