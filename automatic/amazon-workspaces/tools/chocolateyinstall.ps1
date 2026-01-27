623fb3654df8615901044b14ab93531c6e5dc3c5
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-01-27 19:47:48 +0000

    AU: 2 updated - amazon-workspaces pangolin [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/f0fd4d1998333927682a6668cdc8f8466a99601c

--- automatic/amazon-workspaces/tools/chocolateyinstall.ps1
+++ automatic/amazon-workspaces/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url                    = 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
-$checksum               = 'C1E1796C0A65773AAECB26A4A73AE2378D0E05E4E120ACADEFFF6A1BFEC9FC8B'
+$checksum               = '941BCE5C734D45FB05E460DD0927C0FF054FDCDF82CDABB475CCA84EB0A94028'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
--- automatic/pangolin/tools/chocolateyinstall.ps1
+++ automatic/pangolin/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $url64      = 'https://github.com/fosrl/windows/releases/download/0.5.0/pangolin-amd64-0.5.0.msi'
-$checksum64 = ''
+$checksum64 = '67f1a1e9557effdb56f504e7402efd597caa7e7700c63e2fe60374e3a3294b20'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
