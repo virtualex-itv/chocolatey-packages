7ec42f12e34691533febedb5463a3dd8604c8b02
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-02-23 18:12:33 +0000

    AU: 1 updated - displayfusion.portable [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/c7f463fe985693c06c37dc74263d847592bc6206

--- automatic/displayfusion.portable/tools/chocolateyinstall.ps1
+++ automatic/displayfusion.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/101/DisplayFusion-12.0.1-x64.zip'
-$checksum              = '576e4d2cc4239d0174234d41c8ffcfa654ffa7718607314cfdd5c008dc4eb97c'
+$url                   = 'https://binaryfortressdownloads.com/Download/BFSFiles/101/DisplayFusion-12.0.2-x64.zip'
+$checksum              = '3090c550a82cde61bf4b819566e29e29fd0432cc7f70fb0fc9079d1444e66246'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
