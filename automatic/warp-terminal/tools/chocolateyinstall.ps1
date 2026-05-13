063f9c608f6c8831ccf9330d5e8ebdb768ed1ee6
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-05-13 06:09:49 +0000

    AU: 1 updated - warp-terminal [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/d10e6c2f23823525788a4cbf6587329c90239435

--- automatic/warp-terminal/tools/chocolateyinstall.ps1
+++ automatic/warp-terminal/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://releases.warp.dev/stable/v0.2026.05.06.15.42.stable_04/WarpSetup.exe'
-$checksum               = '9773e318bbb6db06d9ce3a2a701fe28f083f47ff365a154fc2cad19795718987'
+$url                    = 'https://releases.warp.dev/stable/v0.2026.05.06.15.42.stable_05/WarpSetup.exe'
+$checksum               = 'abaf264dcb66e5b078b1a35d7190d1f2f1e793f4f7e56a9b626899e02a42d237'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
