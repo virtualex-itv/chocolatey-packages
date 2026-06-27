b6e5f5e9ce106ce9bbbfaebb3a0706ec7967665d
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-06-27 00:10:25 +0000

    AU: 2 updated - pangolin warp-terminal [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/d4f7b631a43a9e77ea21933a72c76381f9721caa

--- automatic/pangolin/tools/chocolateyinstall.ps1
+++ automatic/pangolin/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64      = 'https://github.com/fosrl/windows/releases/download/0.10.6/pangolin-amd64-0.10.6.msi'
-$checksum64 = 'c5925480a949632a5860e6d44400170fab688c6a3576162794f160ccc3500b39'
+$url64      = 'https://github.com/fosrl/windows/releases/download/0.11.0/pangolin-amd64-0.11.0.msi'
+$checksum64 = '382cd4d712390478edc6eb71a0241ff35c61d4ed23687d73459c277dcdd5536f'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
--- automatic/warp-terminal/tools/chocolateyinstall.ps1
+++ automatic/warp-terminal/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://releases.warp.dev/stable/v0.2026.06.17.09.49.stable_02/WarpSetup.exe'
-$checksum               = 'fc1592632fa7c4964ca13b40d93c7f315f6fbdd3ad678cc078d30defc7e4c68e'
+$url                    = 'https://releases.warp.dev/stable/v0.2026.06.24.09.19.stable_02/WarpSetup.exe'
+$checksum               = '82cd7b953398c10e54473bf6280c4d6df0b3a8c2cb699fc7768dee023056a84a'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
