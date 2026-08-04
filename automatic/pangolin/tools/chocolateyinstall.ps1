4eb8c074468390038c9d7692d9b4ba99e84e3fec
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-08-04 18:11:05 +0000

    AU: 1 updated - pangolin [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/131d56a346697703461c2fb358d1209ff311cc4d

--- automatic/pangolin/tools/chocolateyinstall.ps1
+++ automatic/pangolin/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64      = 'https://github.com/fosrl/windows/releases/download/0.13.2/pangolin-amd64-0.13.2.msi'
-$checksum64 = '6252fd1c3f58b4facd0856d5529e6cc3df9fb4f3d6d7e55580c683de832b4103'
+$url64      = 'https://github.com/fosrl/windows/releases/download/0.13.3/pangolin-amd64-0.13.3.msi'
+$checksum64 = '290df5fac58cd8ef0ae745b5dbb700f7a4caba1818b63a0891e09d191971cf74'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
