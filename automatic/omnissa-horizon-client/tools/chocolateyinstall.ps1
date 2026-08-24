55274e32ff8ed39be8c6fb7c188a8b5a914b1780
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-08-24 12:11:46 +0000

    AU: 1 updated - omnissa-horizon-client [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/bbd494834bb658c42dc5c6b9ee4a4a33e19cb790

--- automatic/omnissa-horizon-client/tools/chocolateyinstall.ps1
+++ automatic/omnissa-horizon-client/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                    = 'https://download3.omnissa.com/software/CART27FQ1_WIN_2603/Omnissa-Horizon-Client-2603-8.18.0-24230927696.exe'
-$checksum               = '32a2aa9fe766b329fb2eb50f0cf2946dab73e46fffe19b501993952e99aded25'
+$url                    = 'https://download3.omnissa.com/software/CART27FQ2_WIN_2606/Omnissa-Horizon-Client-2606-8.19.0-32215845441.exe'
+$checksum               = '4e8e54385cd066da229e1f5ae15aad826e90088a351fc34e0bad048fa5b4aa00'
 $checksumType           = 'sha256'
 
 $packageArgs = @{
