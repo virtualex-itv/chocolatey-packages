68bff0e1891a2ba7daea1800bde76167cfffb8ea
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-09-01 17:04:35 +0000

    AU: 1 updated - betterdiscord-cli [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/917b3c77fd8009431157a4acbbbfed3b48f40d1f

--- automatic/betterdiscord-cli/tools/chocolateyInstall.ps1
+++ automatic/betterdiscord-cli/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
-$url64         = ''
-$checksum64    = ''
-$urlArm64      = ''
-$checksumArm64 = ''
+$url64         = 'https://github.com/BetterDiscord/cli/releases/download/v1.0.0/bdcli_1.0.0_windows_amd64.zip'
+$checksum64    = '4cc9ebf30c99311caf72ba2fad90d6c4a5ce2b77be4b83f7de8589d5de023b80'
+$urlArm64      = 'https://github.com/BetterDiscord/cli/releases/download/v1.0.0/bdcli_1.0.0_windows_arm64.zip'
+$checksumArm64 = '60838d7f7e4d6a70c76dedeee1e84bd551f3e32691cad8a690d6c08661131769'
 
 $isArm64 = $env:PROCESSOR_ARCHITECTURE -eq 'ARM64'
 
