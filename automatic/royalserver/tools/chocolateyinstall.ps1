410883596f4900f5f6cb16b21624768493f61fb6
virtualex-itv <12056704+virtualex-itv@users.noreply.github.com>
2026-09-10 12:05:47 +0000

    AU: 2 updated - royalserver vcf-powercli-psmodule [skip ci] https://gist.github.com/virtualex-itv/113649984d5ef59e0ec2d529dfc28df4/869ce023d43ee99b9bb3336f433e74038b43b0d3

--- automatic/royalserver/tools/chocolateyinstall.ps1
+++ automatic/royalserver/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_5.04.50529.0.msi'
-$checksum              = 'c743a9801653de57cea796de47fcac9ef4f42457ded9d0317344e6ba0f567018'
+$url                   = 'https://download.royalapps.com/RoyalServer/RoyalServerInstaller_5.04.50909.0.msi'
+$checksum              = 'a134a2ea416c98b640a5387d7e1c2ba4d2afe5da4f22e8f36cd858c2c5dc7a8c'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
